a3f51cf2ef922b1f6743234cb8a49c81863eaa1f
Jalal Lakhlili <ljala@draco01.mpcdf.mpg.de>
2019-03-18 19:05:58 +0100

    [standalone/src] add chease_cpo routine.

--- standalone/src/chease_standalone.f90
+++ standalone/src/chease_standalone.f90
@@ -15,9 +15,50 @@ module chease_standalone
   end interface
 
 contains
+  
+  ! ... fortran CHEASE wrapper
+  subroutine chease_cpo(equil_in, equil_out) 
+    use iso_c_binding
+    use string_binding
+    use xml_file_reader
+    use deallocate_structures
+    use ifport, only: FULLPATHQQ
 
+    implicit none
+    type (type_equilibrium), pointer :: equil_in(:)
+    type (type_equilibrium), pointer :: equil_out(:)
+    
+    type (type_param) :: code_parameters
 
+    ! Path to the workflows directory
+    character(len=128) :: workflows_dir  
+    integer :: len
+    
+    ! Get code params
+    len = FULLPATHQQ('../../workflows/', workflows_dir)
+    call fill_param(code_parameters, workflows_dir(:len)// 'chease.xml', '', &
+                                     workflows_dir(:len)// 'chease.xsd')
 
+    !...  run CHEASE
+    call chease(equil_in, equil_out, code_parameters)
+
+    ! deallocations
+    if (associated(code_parameters%schema)) then
+       deallocate(code_parameters%schema)
+    endif
+    if (associated(code_parameters%parameters)) then
+       deallocate(code_parameters%parameters)
+    endif
+    if (associated(code_parameters%default_param)) then
+       deallocate(code_parameters%default_param)
+    endif
+
+    print *,"return from fortran wrapper"
+    
+  end subroutine chease_cpo
+  ! ...
+
+  ! ...
   subroutine chease2file(equil_old, aoutput) 
     use iso_c_binding
     use string_binding
@@ -25,11 +66,12 @@ contains
     use write_structures
     use xml_file_reader
     use deallocate_structures
+    use ifport, only: FULLPATHQQ
     implicit none
     type (type_equilibrium), pointer :: equil_old(:)
     real(R8) :: aoutput(:)
     
-    integer :: ios, i, npar
+    integer :: ios, i, npar, len
     integer, save :: cpt = 0
     character(4)  :: cptstr
 
@@ -42,31 +84,32 @@ contains
     character(F_STR_SIZE) :: username, tmpdir
     integer :: tmpsize
 
+    character(len=128) :: workflows_dir  ! Path to the workflows directory
+    
     print *,"fortran CHEASE wrapper"
 
     
     print *,"get code params"
     !...  run CHEASE
-    call fill_param(code_parameters, 'standalone/data/CHEASE/ref/chease.xml', '', &
-                                     'standalone/data/CHEASE/ref/chease.xsd')
+    len = FULLPATHQQ('../../workflows/', workflows_dir)
+    call fill_param(code_parameters, workflows_dir(:len)// 'chease.xml', '', &
+                                     workflows_dir(:len)// 'chease.xsd')
 
     print *,"run chease routine"
     call chease(equil_old, equil_new, code_parameters)
     
     ! Safety factor
-   ! aoutput(:)=equil_new(1)%profiles_1d%q
+    aoutput(:)=equil_new(1)%profiles_1d%q
 
     ! transfer CPO to buf
     !...  write the results
-!    write(cptstr,'(I4.4)') init_step+cpt
-!    cpt = cpt+1
-!
-!    !equil_file_out = 'chease_equilibrium_'//cptstr//'.cpo'
-!    equil_file_out = 'chease_equilibrium_new.cpo'
-!    call open_write_file(11,equil_file_out)
-!    call write_cpo(equil_new(1),'equilibrium')
-!    call close_write_file
-!    call deallocate_cpo(equil_old)
+    write(cptstr,'(I4.4)') init_step+cpt
+    cpt = cpt+1
+    equil_file_out = 'chease_equilibrium_'//cptstr//'.cpo'
+    call open_write_file(18,equil_file_out)
+    call write_cpo(equil_new(1),'equilibrium')
+    call close_write_file
+    call deallocate_cpo(equil_old)
     call deallocate_cpo(equil_new)
 
 
@@ -81,9 +124,8 @@ contains
     endif
 
     print *,"return from fortran wrapper"
-    
 
   end subroutine chease2file
-
+  ! ...
 
 end module chease_standalone
