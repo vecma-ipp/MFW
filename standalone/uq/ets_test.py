a077bb50d9e677632bab2ee5de2d6a9344c9b3ee
=Jalal Lakhlili <jlakhlili@gmail.com>
2019-05-09 09:25:06 +0200

    [standalone] update uq  ets_test.

--- standalone/uq/ets_test.py
+++ standalone/uq/ets_test.py
@@ -18,8 +18,7 @@ start_time = time.time()
 cpo_dir = os.path.abspath("../data/AUG_28906_5/BGB_GEM_SPREAD/4FT/")
 
 # Uncertain parameters
-#uncert_params = ["D1", "D2", "D3", "D4"]
-uncert_params = ["Te0"]
+uncert_params = ["D1", "D2", "D3", "D4"]
 
 # To store input/ouput files
 tmp_dir = "/ptmp/ljala/"
@@ -41,7 +40,7 @@ ets_campaign = uq.Campaign(
 
 campaign_dir = ets_campaign.campaign_dir
 
-# Copy xml files needed in the ets code
+# Copy xml files needed in the ETS code
 os.system("mkdir " + campaign_dir +"/workflows")
 os.system("cp ../../workflows/ets.xml "+ campaign_dir +"/workflows")
 os.system("cp ../../workflows/ets.xsd "+ campaign_dir +"/workflows")
@@ -51,16 +50,9 @@ common_dir = campaign_dir +"/common/"
 os.system("cp " + cpo_dir + "/*.cpo " + common_dir)
 
 # Get uncertain parameters distrubutions
-corep_file = common_dir + "ets_coreprof_in.cpo"
-corep = read(corep_file, "coreprof")
-diff_eff = [corep.te.boundary.value[0]]
-
-print('>>>>>>>>>>>> Te0 = ', diff_eff[0])
-#coret_file = common_dir + "ets_coretransp_in.cpo"
-#coret      = read(coret_file, "coretransp")
-n_params = len(uncert_params)
-#diff_eff   = coret.values[0].te_transp.diff_eff
-list_dist  = [cp.Normal(diff_eff[i], 0.2*diff_eff[i]) for i in range(n_params)]
+coret_file = common_dir + "ets_coretransp_in.cpo"
+coret      = read(coret_file, "coretransp")
+list_dist  = [cp.Normal(diff_eff[i], 0.2*diff_eff[i]) for i in range(4)]
 
 # Define the parameters dictionary
 for i in range(n_params):
@@ -79,7 +71,7 @@ ets_campaign.apply_for_each_run_dir(uq.actions.ExecuteLocal(cmd))
 
 # Aggregate the results from all runs.
 output_filename = ets_campaign.params_info['out_file']['default']
-output_columns = ['te']
+output_columns = ['te', 'ti', 'p']
 
 aggregate = uq.elements.collate.AggregateSamples(
     ets_campaign,
@@ -93,30 +85,26 @@ aggregate.apply()
 # Analysis
 analysis = uq.elements.analysis.PCEAnalysis(
     ets_campaign, value_cols=output_columns)
-aname = analysis.element_name()
+
 analysis.apply()
 
 # Results
 stat = analysis.statistical_moments('te')
-#pctl = analysis.percentiles('te')
+pctl = analysis.percentiles('te')
 sobols = analysis.sobol_indices('te', 'first_order')
 
 # Elapsed time
 end_time = time.time()
-print('>>>>> elapsed time = ', (end_time - start_time)/60.)
 
-#corep_file = common_dir + '/ets_coreprof_in.cpo'
-#corep = read(corep_file, 'coreprof')
+# For plots
+corep_file = common_dir + '/ets_coreprof_in.cpo'
+corep = read(corep_file, 'coreprof')
 rho = corep.rho_tor
-
-#equil_file = common_dir + 'ets_equilibrium_in.cpo'
-#equil = read(equil_file, 'equilibrium')
-#rho = equil.profiles_1d.rho_tor
 
 #  Graphics for descriptive satatistics
 plots.plot_stats(rho, stat,
                  xlabel=r'$\rho_{tor} ~ [m]$', ylabel='Te',
                  ftitle='UQP1 - ETS output: Te profile',
-                 fname='te_ci.png')
+                 fname='te_profile.png')
 
 plots.plot_sobols(rho, sobols, uncert_params)
