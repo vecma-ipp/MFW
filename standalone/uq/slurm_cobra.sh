562e098154ecaa09ff045c9a52edb1bc7952740c
Jalal Lakhlili <jlakhlili@gmail.com>
2019-08-26 13:37:30 +0200

    Update .gitignore

--- standalone/uq/slurm_cobra.sh
+++ standalone/uq/slurm_cobra.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -l
 
 ## job name
-#SBATCH --job-name=easyvvuq_pj
+#SBATCH --job-name=ets_uq
 
 ## stdout and stderr files
-#SBATCH --output=test.out.%j
-#SBATCH --error=test.err.%j
+#SBATCH --output=logs/test.out.%j
+#SBATCH --error=logs/test.err.%j
 
 ## wall time in format MINUTES:SECONDS
-#SBATCH --time=03:00:00
+#SBATCH --time=10:00:00
 
 ## number of nodes and tasks per node
 #SBATCH --nodes=1
@@ -21,4 +21,4 @@
 #SBATCH --mail-user=ljala@rzg.mpg.de
 
 # Run the program:.
-python test_uq_ets.py > test.log.${SLURM_JOBID}
+python test_uq1_ets.py > logs/test.log.${SLURM_JOBID}
