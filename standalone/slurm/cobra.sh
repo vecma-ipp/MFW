42d08e70970992279e03e41f2d2afb65d411a804
Jalal Lakhlili <jlakhlili@gmail.com>
2019-09-19 16:33:40 +0200

    Add slurm scripts

---
+++ standalone/slurm/cobra.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -l
+
+## job name
+#SBATCH --job-name=ets_uq
+
+## stdout and stderr files
+#SBATCH --output=logs/test.out.%j
+#SBATCH --error=logs/test.err.%j
+
+## wall time in format MINUTES:SECONDS
+#SBATCH --time=02:00:00
+
+## number of nodes and tasks per node
+#SBATCH --nodes=1
+#SBATCH --tasks-per-node=40
+
+## queue name
+#SBATCH --partition=medium
+
+## grant
+#SBATCH --mail-user=ljala@rzg.mpg.de
+
+# Run the program:.
+python test_initial_cond.py > logs/test.log.${SLURM_JOBID}
