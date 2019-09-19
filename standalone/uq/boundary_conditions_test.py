7246d18b3567d91009e130ca8b00d6fcb0f0cbb5
Jalal Lakhlili <jlakhlili@gmail.com>
2019-09-19 17:47:26 +0200

    Update UQ tests

--- standalone/uq/boundary_conditions_test.py
+++ standalone/uq/boundary_conditions_test.py
@@ -1,6 +1,5 @@
 import os, sys
 import time
-import pandas as pd
 import chaospy as cp
 import easyvvuq as uq
 import matplotlib.pylab as plt
@@ -10,7 +9,8 @@ from tools import plots, cpo_template
 # Boundary conditions test:
 # UQ for a given model(s) using Non intrisive method.
 # Uncertainties in Te and Ti boudaries (Edge)
-.
+
+# For Ellapsed time
 time0 = time.time()
 
 # OS env
@@ -43,7 +43,30 @@ uncertain_params = {
 output_columns = ["Te", "Ti"]
 
 # Initialize Campaign object
-print('Initialize Campaign object')
+print('>>> Initialize Campaign object')
+my_campaign = uq.Campaign(name = 'bc_uq', work_dir=tmp_dir)
+
+# Copy XML files needed in the ETS wrappers
+campaign_dir = my_campaign.campaign_dir
+os.system("mkdir " + campaign_dir +"/workflows")
+os.system("cp ../../workflows/*.xml "+ campaign_dir +"/workflows")
+os.system("cp ../../workflows/*.xsd "+ campaign_dir +"/workflows")
+
+# Copy input CPO files in common directory
+common_dir = campaign_dir +"/common/"
+os.system("mkdir " + common_dir)
+os.system("cp " + cpo_dir + "/*.cpo " + common_dir)
+
+# Create the encoder and get the app parameters
+print('>>> Create the encoder')
+input_filename = "ets_coreprof_in.cpo"
+encoder = cpo_template.CPOEncoder(template_filename=input_filename,
+                                  template_cponame="coreprof",
+                                  cpos_directory=common_dir,# For the output: quantities of intersts
+output_columns = ["Te", "Ti"]
+
+# Initialize Campaign object
+print('>>> Initialize Campaign object')
 my_campaign = uq.Campaign(name = 'uq_ets', work_dir=tmp_dir)
 
 # Copy XML files needed in the ETS wrappers
@@ -58,11 +81,89 @@ os.system("mkdir " + common_dir)
 os.system("cp " + cpo_dir + "/*.cpo " + common_dir)
 
 # Create the encoder and get the app parameters
-print('Create the encoder')
+print('>>> Create the encoder')
 input_filename = "ets_coreprof_in.cpo"
 encoder = cpo_template.CPOEncoder(template_filename=input_filename,
                                   template_cponame="coreprof",
                                   cpos_directory=common_dir,
+                                  target_filename=input_filename,
+                                  uncertain_params=uncertain_params
+                                 )
+
+params, vary = encoder.draw_app_params()
+
+# Create the encoder
+print('Create the encoder')
+output_filename = "ets_coreprof_out.cpo"
+decoder = cpo_template.CPODecoder(target_filename=output_filename,
+                                  target_cponame="coreprof",
+                                  output_columns=output_columns)
+
+# Add the ETS app (automatically set as current app)
+print('>>> Add app to campagn object')
+my_campaign.add_app(name="uq_ets",
+                    params=params,
+                    encoder=encoder,
+                    decoder=decoder
+                    )
+
+# Create a collation element for this campaign
+print('>>> Create Collater')
+collater = uq.collate.AggregateSamples(average=False)
+my_campaign.set_collater(collater)
+
+# Create the sampler
+print('>>> Create the sampler')
+my_sampler = uq.sampling.PCESampler(vary=vary,
+                                    polynomial_order=4,
+                                    quadrature_rule='G',
+                                    sparse=False
+                                    )
+my_campaign.set_sampler(my_sampler)
+
+# Will draw all (of the finite set of samples)
+print('>>> Draw Samples')
+my_campaign.draw_samples()
+
+print('>>> Populate runs_dir')
+my_campaign.populate_runs_dir()
+
+print('>>> Execute BlackBox code')
+my_campaign.apply_for_each_run_dir(uq.actions.ExecuteLocal(bbox_exe))
+
+print('>>> Collate')
+my_campaign.collate()
+
+# Post-processing analysis
+print('>>> Post-processing analysis')
+analysis = uq.analysis.PCEAnalysis(sampler=my_sampler, qoi_cols=output_columns)
+my_campaign.apply_analysis(analysis)
+
+print('>>> Get results')
+results = my_campaign.get_last_analysis()
+
+# Get Descriptive Statistics
+print('>>> Get Descriptive Statistics')
+stats_te = results['statistical_moments']['Te']
+pctl_te = results['percentiles']['Te']
+stot_te = results['sobols_total']['Te']
+
+stats_ti = results['statistical_moments']['Ti']
+pctl_ti = results['percentiles']['Ti']
+stot_ti = results['sobols_total']['Ti']
+
+print('>>> Ellapsed time: ', time.time() - time0)
+
+#  Graphics for descriptive satatistics
+print('>>> Statictics and SA plots')
+corep = read(os.path.join(cpo_dir,  "ets_coreprof_in.cpo"), "coreprof")
+rho = corep.rho_tor
+uncertain_params = ["Te_boundary", "Ti_boundary"]
+plots.plot_stats_pctl(rho, stats_te, pctl_te,
+                 xlabel=r'$\rho_{tor} ~ [m]$', ylabel=r'$Te$',
+                 ftitle='Te profile',
+                 fname='figs/te_ets_stats1')
+
                                   target_filename=input_filename,
                                   uncertain_params=uncertain_params
                                  )
@@ -77,7 +178,7 @@ decoder = cpo_template.CPODecoder(target_filename=output_filename,
                                   output_columns=output_columns)
 
 # Add the ETS app (automatically set as current app)
-print('Add app to campagn object')
+print('>>> Add app to campagn object')
 my_campaign.add_app(name="uq_ets",
                     params=params,
                     encoder=encoder,
@@ -85,12 +186,12 @@ my_campaign.add_app(name="uq_ets",
                     )
 
 # Create a collation element for this campaign
-print('Create Collater')
+print('>>> Create Collater')
 collater = uq.collate.AggregateSamples(average=False)
 my_campaign.set_collater(collater)
 
 # Create the sampler
-print('Create the sampler')
+print('>>> Create the sampler')
 my_sampler = uq.sampling.PCESampler(vary=vary,
                                     polynomial_order=4,
                                     quadrature_rule='G',
@@ -99,27 +200,28 @@ my_sampler = uq.sampling.PCESampler(vary=vary,
 my_campaign.set_sampler(my_sampler)
 
 # Will draw all (of the finite set of samples)
-print('Draw Samples')
+print('>>> Draw Samples')
 my_campaign.draw_samples()
 
-print('Populate runs_dir')
+print('>>> Populate runs_dir')
 my_campaign.populate_runs_dir()
 
-print('Execute BlackBox code')
+print('>>> Execute BlackBox code')
 my_campaign.apply_for_each_run_dir(uq.actions.ExecuteLocal(bbox_exe))
 
-print('Collate')
+print('>>> Collate')
 my_campaign.collate()
 
 # Post-processing analysis
-print('Post-processing analysis')
+print('>>> Post-processing analysis')
 analysis = uq.analysis.PCEAnalysis(sampler=my_sampler, qoi_cols=output_columns)
 my_campaign.apply_analysis(analysis)
 
+print('>>> Get results')
 results = my_campaign.get_last_analysis()
 
 # Get Descriptive Statistics
-print('Get Descriptive Statistics')
+print('>>> Get Descriptive Statistics')
 stats_te = results['statistical_moments']['Te']
 pctl_te = results['percentiles']['Te']
 stot_te = results['sobols_total']['Te']
@@ -128,11 +230,10 @@ stats_ti = results['statistical_moments']['Ti']
 pctl_ti = results['percentiles']['Ti']
 stot_ti = results['sobols_total']['Ti']
 
-print('Ellapsed time: ', time.time() - time0)
-
+print('>>> Ellapsed time: ', time.time() - time0)
 
 #  Graphics for descriptive satatistics
-print('PLOTS')
+print('>>> Statictics and SA plots')
 corep = read(os.path.join(cpo_dir,  "ets_coreprof_in.cpo"), "coreprof")
 rho = corep.rho_tor
 uncertain_params = ["Te_boundary", "Ti_boundary"]
@@ -155,14 +256,4 @@ plots.plot_sobols(rho, stot_ti, uncertain_params,
                   ftitle=' Total-Order Sobol indices - QoI: Ti',
                   fname='figs/ti_ets_stot1')
 
-#print('TO database')
-## To create new table for results and store them in the data base
-#engine = my_campaign.campaign_db.engine
-#stats_df = pd.DataFrame.from_dict(stats)
-#stats_df.to_sql('STATSP', engine, if_exists='append')
-#sob_1st_df = pd.dataframe.from_dict(sob_1st)
-#sob_1st_df.to_sql('SOBOLS_1_P', engine, if_exists='append')
-#sob_tot_df = pd.DataFrame.from_dict(sob_tot)
-#sob_tot_df.to_sql('SOBOLS_TOT_P', engine, if_exists='append')
-
-print('=== End of test_uq1_ets ===')
+print('>>> End of boundary_conditions_test.')
--- standalone/uq/gaussian_sources_test.py
+++ standalone/uq/gaussian_sources_test.py
@@ -1,17 +1,16 @@
 import os, sys
 import time
-import pandas as pd
 import chaospy as cp
 import easyvvuq as uq
 import matplotlib.pylab as plt
 from ascii_cpo import read
 from tools import plots, cpo_template
 
-# Initial Conditions test:
+# Boundary conditions test:
 # UQ for a given model(s) using Non intrisive method.
-# Uncertainties in Te and Ti boudaries (Edge)
+# Uncertainties in Sources
 
-
+# For Ellapsed time
 time0 = time.time()
 
 # OS env
@@ -28,15 +27,39 @@ bbox_exe = os.path.abspath("../bin/"+SYS+"/loop_test1 ")
 
 # Define a specific parameter space
 uncertain_params = {
-    "Te_boundary": {
-        "type": "float",
-        "distribution": "Normal",
-        "margin_error": 0.25,
+    "electrons_heating": {
+        "amplitude":{
+            "type": "float",
+            "distribution": "Uniform",
+            "margin_error": 0.2,
+        }
+        "position":{
+            "type": "float",
+            "distribution": "Uniform",
+            "margin_error": 0.25,
+        }
+        "width":{
+            "type": "float",
+            "distribution": "Uniform",
+            "margin_error": 0.2,
+        }
     },
-    "Ti_boundary": {
-        "type": "float",
-        "distribution": "Normal",
-        "margin_error": 0.25,
+    "ions_heating": {
+        "amplitude":{
+            "type": "float",
+            "distribution": "Uniform",
+            "margin_error": 0.25,
+        }
+        "position":{
+            "type": "float",
+            "distribution": "Uniform",
+            "margin_error": 0.25,
+        }
+        "width":{
+            "type": "float",
+            "distribution": "Uniform",
+            "margin_error": 0.2,
+        }
     }
 }
 
@@ -44,8 +67,8 @@ uncertain_params = {
 output_columns = ["Te", "Ti"]
 
 # Initialize Campaign object
-print('Initialize Campaign object')
-my_campaign = uq.Campaign(name = 'uq_ets', work_dir=tmp_dir)
+print('>>> Initialize Campaign object')
+my_campaign = uq.Campaign(name = 'gs_uq', work_dir=tmp_dir)
 
 # Copy XML files needed in the ETS wrappers
 campaign_dir = my_campaign.campaign_dir
@@ -59,7 +82,7 @@ os.system("mkdir " + common_dir)
 os.system("cp " + cpo_dir + "/*.cpo " + common_dir)
 
 # Create the encoder and get the app parameters
-print('Create the encoder')
+print('>>> Create the encoder')
 input_filename = "ets_coreprof_in.cpo"
 encoder = cpo_template.CPOEncoder(template_filename=input_filename,
                                   template_cponame="coreprof",
@@ -72,13 +95,13 @@ params, vary = encoder.draw_app_params()
 
 # Create the encoder
 print('Create the encoder')
-output_filename = "ets_coreprof_0020.cpo"
+input_filename = "source_dummy.xml"
 decoder = cpo_template.CPODecoder(target_filename=output_filename,
                                   target_cponame="coreprof",
                                   output_columns=output_columns)
 
 # Add the ETS app (automatically set as current app)
-print('Add app to campagn object')
+print('>>> Add app to campagn object')
 my_campaign.add_app(name="uq_ets",
                     params=params,
                     encoder=encoder,
@@ -86,12 +109,12 @@ my_campaign.add_app(name="uq_ets",
                     )
 
 # Create a collation element for this campaign
-print('Create Collater')
+print('>>> Create Collater')
 collater = uq.collate.AggregateSamples(average=False)
 my_campaign.set_collater(collater)
 
 # Create the sampler
-print('Create the sampler')
+print('>>> Create the sampler')
 my_sampler = uq.sampling.PCESampler(vary=vary,
                                     polynomial_order=4,
                                     quadrature_rule='G',
@@ -100,28 +123,28 @@ my_sampler = uq.sampling.PCESampler(vary=vary,
 my_campaign.set_sampler(my_sampler)
 
 # Will draw all (of the finite set of samples)
-print('Draw Samples')
+print('>>> Draw Samples')
 my_campaign.draw_samples()
 
-print('Populate runs_dir')
+print('>>> Populate runs_dir')
 my_campaign.populate_runs_dir()
 
-print('Execute BlackBox code')
+print('>>> Execute BlackBox code')
 my_campaign.apply_for_each_run_dir(uq.actions.ExecuteLocal(bbox_exe))
 
-print('Collate')
+print('>>> Collate')
 my_campaign.collate()
 
 # Post-processing analysis
-print('Post-processing analysis')
+print('>>> Post-processing analysis')
 analysis = uq.analysis.PCEAnalysis(sampler=my_sampler, qoi_cols=output_columns)
-
 my_campaign.apply_analysis(analysis)
 
+print('>>> Get results')
 results = my_campaign.get_last_analysis()
 
 # Get Descriptive Statistics
-print('Get Descriptive Statistics')
+print('>>> Get Descriptive Statistics')
 stats_te = results['statistical_moments']['Te']
 pctl_te = results['percentiles']['Te']
 stot_te = results['sobols_total']['Te']
@@ -130,11 +153,10 @@ stats_ti = results['statistical_moments']['Ti']
 pctl_ti = results['percentiles']['Ti']
 stot_ti = results['sobols_total']['Ti']
 
-
-print('Ellapsed time: ', time.time() - time0)
+print('>>> Ellapsed time: ', time.time() - time0)
 
 #  Graphics for descriptive satatistics
-print('PLOTS')
+print('>>> Statictics and SA plots')
 corep = read(os.path.join(cpo_dir,  "ets_coreprof_in.cpo"), "coreprof")
 rho = corep.rho_tor
 uncertain_params = ["Te_boundary", "Ti_boundary"]
@@ -157,14 +179,4 @@ plots.plot_sobols(rho, stot_ti, uncertain_params,
                   ftitle=' Total-Order Sobol indices - QoI: Ti',
                   fname='figs/ti_ets_stot1')
 
-#print('TO database')
-## To create new table for results and store them in the data base
-#engine = my_campaign.campaign_db.engine
-#stats_df = pd.DataFrame.from_dict(stats)
-#stats_df.to_sql('STATSP', engine, if_exists='append')
-#sob_1st_df = pd.dataframe.from_dict(sob_1st)
-#sob_1st_df.to_sql('SOBOLS_1_P', engine, if_exists='append')
-#sob_tot_df = pd.DataFrame.from_dict(sob_tot)
-#sob_tot_df.to_sql('SOBOLS_TOT_P', engine, if_exists='append')
-
-print('=== End of test_uq1_ets ===')
+print('>>> End of boundary_conditions_test.')
