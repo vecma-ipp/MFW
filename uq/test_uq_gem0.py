46ffbfc6505191ce6b5ce6be70eeebb1d9eeaf58
Jalal Lakhlili <jlakhlili@gmail.com>
2020-01-21 18:21:40 +0100

    Udpate gem0 test

--- uq/test_uq_gem0.py
+++ uq/test_uq_gem0.py
@@ -117,7 +117,7 @@ my_campaign.add_app(name=campaign_name,
 
 # Create the sampler
 print('>>> Create the sampler')
-my_sampler = uq.sampling.PCESampler(vary=vary, polynomial_order=3)
+my_sampler = uq.sampling.PCESampler(vary=vary, polynomial_order=2)
 my_campaign.set_sampler(my_sampler)
 
 # Will draw all (of the finite set of samples)
