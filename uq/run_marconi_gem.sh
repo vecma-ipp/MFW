a5cb84efe0ca714303624f78fd13843a8b5dddf3
Yehor Yudin <yyudin00@r000u07l02.marconi.cineca.it>
2020-08-04 11:12:26 +0200

    Added slurm batch files for the SA campaings of GEM and GEM0
     - run GEM0 for PCE, QMC, different param-s sets, param-s distribution
     - more debug prints

---
+++ uq/run_marconi_gem.sh
@@ -0,0 +1,43 @@
+#!/bin/bash -l
+
+## job name
+#SBATCH --job-name=UQ_GEM_F
+
+## stdout and stderr files
+#SBATCH --output=test-out.%j.gem
+#SBATCH --error=test-err.%j.gem
+
+## wall time in format MINUTES:SECONDS
+#SBATCH --time=4:00:00
+
+## number of nodes and tasks per node
+#SBATCH --nodes=4
+#SBATCH --ntasks-per-node=48
+###SBATCH --ntasks-per-core=1
+###SBATCH --cpus-per-task=1
+
+###SBATCH --partition=medium
+#SBATCH --partition=skl_fua_prod
+###SBATCH --qos=
+
+## grant
+#SBATCH --account=FUA34_UQMWA2
+#SBATCH --mail-type=END
+#SBATCH --mail-user=yyudin@ipp.mpg.de
+
+export SYS=MARCONI
+export SCRATCH=$CINECA_SCRATCH
+export PYTHONPATH=$HOME/.local/lib/python3.6/site-packages/easyvvuq:$PYTHONPATH
+
+export MPICMD=mpiexec
+###export LD_LIBRARY_PATH=${FFTW_HOME}/lib:${LD_LIBRARY_PATH}
+
+# For QCG-PilotJob usage
+ENCODER_MODULES="mfw.templates.cpo_encoder;mfw.templates.xml_encoder"
+export ENCODER_MODULES
+export EASYPJ_CONFIG=conf.sh
+
+# Run the UQ code
+scontrol show --detail job $SLURM_JOBID
+python3 tests/gem_sample.py > test-log.${SLURM_JOBID}.gem
+
---
+++ uq/run_marconi_gem0_sample.sh
@@ -0,0 +1,50 @@
+#!/bin/bash -l
+
+## job name
+#SBATCH --job-name=UQ_GEM0_PCE_8ft
+
+## stdout and stderr files
+#SBATCH --output=test-out.%j.8ft
+#SBATCH --error=test-err.%j.8ft
+
+## wall time in format MINUTES:SECONDS
+#SBATCH --time=1:00:00
+
+## number of nodes and tasks per node
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=48
+###SBATCH --ntasks-per-core=1
+###SBATCH --cpus-per-task=1
+
+###SBATCH --partition=medium
+#SBATCH --partition=skl_fua_prod
+###SBATCH --qos=
+
+## grant
+#SBATCH --account=FUA34_UQMWA2
+#SBATCH --mail-type=END
+#SBATCH --mail-user=yyudin@ipp.mpg.de
+
+export SYS=MARCONI
+export SCRATCH=$CINECA_SCRATCH
+export MPICMD=srun
+###export LD_LIBRARY_PATH=${FFTW_HOME}/lib:${LD_LIBRARY_PATH}
+###export PYTHONPATH=$HOME/.local/lib/python3.6/site-packages/easyvvuq:$PYTHONPATH
+export PYTHONPATH=$HOME/.local/lib/python3.6/site-packages/easyvvuq:$HOME/code/ual_python_interface
+###export PATH=$HOME/.local/lib/python3.6/site-packages/easyvvuq:$PATH
+
+# For QCG-PilotJob usage
+ENCODER_MODULES="mfw.templates.cpo_encoder;mfw.templates.xml_encoder"
+export ENCODER_MODULES
+export EASYPJ_CONFIG=conf.sh
+
+# Run the UQ code
+echo
+echo $PYTHONPATH
+echo
+echo $PATH
+echo
+scontrol show --detail job $SLURM_JOBID
+echo
+python3 tests/gem0_sample.py > test-log.${SLURM_JOBID}.8ft
+
