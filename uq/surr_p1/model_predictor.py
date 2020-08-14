ced69d3eaf4b67913a4226c9c43a96aab7dc3222
Yehor Yudin <yehor.yudin@ipp.mpg.de>
2020-08-14 11:20:47 +0200

    Fixed model predictor file to re-write ouput CPO-s

--- uq/surr_p1/model_predictor.py
+++ uq/surr_p1/model_predictor.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import os
 #import timeit
@@ -29,30 +29,35 @@ def load_data(corep_file, flux_tube_index):
     ti_ddrho = corep.ti.ddrho[flux_tube_index]
 
     # outputs (1 flux tube)
-    # coret = read(coret_file, 'coretransp')
-    # te_transp_flux = coret.values[0].te_transp.flux[0]
-    # ti_transp_flux = coret.values[0].ti_transp.flux[0]
+    #print(coret_file)
+    #coret = read(coret_file, 'coretransp')
+    #te_transp_flux = coret.values[0].te_transp.flux[0]
+    #ti_transp_flux = coret.values[0].ti_transp.flux[0]
+    #print('Original te_flux value is: {0}'.format(te_transp_flux))
 
     return te_value, ti_value, te_ddrho, ti_ddrho
 
 
 def save_data(coret_file, value):
+    #print("reading cpos")
     cpo = CPOElement(coret_file, "coretransp")
-    print(cpo.core)
-    cpo.set_value("te_transp.flux", value[0])
-    cpo.set_value("ti_transp.flux", value[1])
+    #print(cpo.core)
+    cpo.set_value("te_transp.flux", [value[0]])
+    cpo.set_value("ti_transp.flux", [value[1]])
+    #print("set the te_fl and ti_fl value in the cpo file")
+    #print('New te_flux value is: {0}'.format(value[0]))
     cpo.save(coret_file)
 
 # Main
 if __name__ == "__main__":
 
-    WORKDIR = os.environ['WORKDIR']
-    data_dir = WORKDIR + "/Fusion_Inputs/UQ_GEM_Data/runs/"
+    #WORKDIR = os.environ['WORKDIR']
+    #data_dir = WORKDIR + "/Fusion_Inputs/UQ_GEM_Data/runs/"
 
     flux_tube_index = 69  #TODO has to be read from seomwhere
     #N_runs = 625
-    dim_pred_sample = 1
-    input_dim = 4
+    dim_pred_sample = 1 #TODO read number of files/foders in test output, or passed for EasyVVUQ?
+    input_dim = 4  #TODO read from the model object or from EasyVVUQ
     input_samples = np.zeros((dim_pred_sample, input_dim))
 
     output_dim = 2
@@ -70,7 +75,7 @@ if __name__ == "__main__":
         #output_samples[run] = te_transp_flux, ti_transp_flux
 
     X = input_samples[...]
-
+    #print(X)
     #start = timeit.timeit()
     #n_features = 4
     #kernel = Matern(length_scale=[100, 100, 100, 100], nu=0.5) + RBF(length_scale=[100, 100, 100, 100])
@@ -82,11 +87,10 @@ if __name__ == "__main__":
 
     gpr = load(model_path)
 
-    prediction_y = gpr.predict(input_samples)
-    #print(prediction_y)
+    prediction_y = gpr.predict(X)
 
     for run in range(dim_pred_sample):
         coret_file = "gem_coretransp_out.cpo"
-        save_data(coret_file, prediction_y[run])
+        save_data(coret_file, prediction_y[run, :])
 
     print("> Prediction finished")
