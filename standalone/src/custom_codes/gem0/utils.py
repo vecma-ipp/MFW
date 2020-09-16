b40054faae900dbbf48db3fca655067bb1cf5e92
Yehor Yudin <yehor.yudin@ipp.mpg.de>
2020-09-16 18:48:12 +0200

    Some changes in interpolation routines at code, to be checked

--- standalone/src/custom_codes/gem0/utils.py
+++ standalone/src/custom_codes/gem0/utils.py
@@ -17,6 +17,8 @@ def l3interp(y_in, x_in, nr_in, y_out, x_out, nr_out):
 
     j1 = jstart
 
+    print('the iteration for interpolation is over {}; {}; {}'.format(jfirst, jlast, jstep))
+
     for j in range(jfirst, jlast, jstep):
         if isinstance(x_out, float) == 1:
             x = x_out
@@ -41,7 +43,12 @@ def l3interp(y_in, x_in, nr_in, y_out, x_out, nr_out):
         aint1 = (x - xm) * (x - x0) * (x - x2) / ((x1 - xm) * (x1 - x0) * (x1 - x2))
         aint2 = (x - xm) * (x - x0) * (x - x1) / ((x2 - xm) * (x2 - x0) * (x2 - x1))
 
-        y_out[j] = aintm * y_in[jm] + aint0 * y_in[j0] + aint1 * y_in[j1] + aint2 * y_in[j2]
+        print('y_in lentgth is {} '.format(len(y_in)))
+
+        if isinstance(y_in, float) or isinstance(y_out, float):
+            y_out = aintm * y_in + aint0 * y_in + aint1 * y_in + aint2 * y_in
+        else:
+            y_out[j] = aintm * y_in[jm] + aint0 * y_in[j0] + aint1 * y_in[j1] + aint2 * y_in[j2]
 
 
 def l3deriv(y_in, x_in, nr_in, dydx_out, x_out, nr_out):
@@ -83,4 +90,7 @@ def l3deriv(y_in, x_in, nr_in, dydx_out, x_out, nr_out):
         aint1 = ((x - x0) * (x - x2) + (x - xm) * (x - x2) + (x - xm) * (x - x0)) / ((x1 - xm) * (x1 - x0) * (x1 - x2))
         aint2 = ((x - x0) * (x - x1) + (x - xm) * (x - x1) + (x - xm) * (x - x0)) / ((x2 - xm) * (x2 - x0) * (x2 - x1))
 
-        dydx_out[j] = aintm * y_in[jm] + aint0 * y_in[j0] + aint1 * y_in[j1] + aint2 * y_in[j2]
+        if isinstance(y_in, float):
+            dydx_out = aintm * y_in + aint0 * y_in + aint1 * y_in + aint2 * y_in
+        else:
+            dydx_out[j] = aintm * y_in[jm] + aint0 * y_in[j0] + aint1 * y_in[j1] + aint2 * y_in[j2]
