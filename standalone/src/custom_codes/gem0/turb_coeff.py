a322a6c66008ea2660a4e8829bd82d6741e9d178
Yehor Yudin <yehor.yudin@ipp.mpg.de>
2020-09-16 18:47:35 +0200

    Some comments on gem0 parameters

--- standalone/src/custom_codes/gem0/turb_coeff.py
+++ standalone/src/custom_codes/gem0/turb_coeff.py
@@ -6,9 +6,10 @@ write_diags = True
 write_cpos = True
 hmode = True
 
-thresh = 6.
-beta_reduction = 10.
-etae_pinch = 3.
-chi_d = 3.
-chiratio_phi = 0.7
-ra0 = 0.7
+thresh = 6. # turbulence effects threshold; check range: 5.-7. ; NOT USED CURRENTLY
+beta_reduction = 10. # reduction for high transport; check range: 8-12 ; NOT USED CURRENTLY
+etae_pinch = 3. # for some ratio of gradient the pinch is created; might be only for particles; check range: 2.8-3.2
+chi_d = 3. # normalisation or scaling; check range: 2.5
+chiratio_phi = 0.7 # rotation transport coefficient; might be not need, or only for ions; check range: 0.5-1.0
+
+ra0 = 0.7 # (desired) flux tube coordinate in case of single flux tube
