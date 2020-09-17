0031b0fca7f03bd0b107843cc37ed6cd828b9f1e
Yehor Yudin <yehor.yudin@ipp.mpg.de>
2020-09-17 17:30:32 +0200

    Implemented array definition for coretransp cpo at gem0

--- standalone/src/custom_codes/gem0/turb_constructor.py
+++ standalone/src/custom_codes/gem0/turb_constructor.py
@@ -1,7 +1,10 @@
+# IT IS A PART OF LIBBDS mod_turb
 
 #import ITM_Types
 #import Euitm_schemas
-#import coretransp_types
+
+from ual.coretransp import coretransp
+from ual.coretransp import valuesstruct_arraycoretransp_valuesObj
 
 import numpy as np
 
@@ -9,41 +12,29 @@ def turb_constructor(coretransp, nrho0, nrho, nion):
 
     # allocate transport structure
 
-     #ALLOCATE(coretransp.values)
+    #print('nrho0: {}; nrho: {}; nion: {} '.format(nrho0, nrho, nion))
 
-     coretransp.values.sigma = np.array((nrho - nrho0))
+    coretransp.values.array.append(valuesstruct_arraycoretransp_valuesObj())
 
-     # ALLOCATE(coretransp.values.rho_tor(nrho0:nrho))
-     # ALLOCATE(coretransp.values.rho_tor_norm(nrho0:nrho))
-     # ALLOCATE(coretransp.values.ne_transp.flux(nrho0:nrho))
-     # ALLOCATE(coretransp.values.te_transp.flux(nrho0:nrho))
-     # ALLOCATE(coretransp.values.ni_transp.flux(nrho0:nrho,nion))
-     # ALLOCATE(coretransp.values.ti_transp.flux(nrho0:nrho,nion))
-     # ALLOCATE(coretransp.values.vtor_transp.flux(nrho0:nrho,nion))
-     # ALLOCATE(coretransp.values.ne_transp.diff_eff(nrho0:nrho,3))
-     # ALLOCATE(coretransp.values.te_transp.diff_eff(nrho0:nrho))
-     # ALLOCATE(coretransp.values.ni_transp.diff_eff(nrho0:nrho,nion,3))
-     # ALLOCATE(coretransp.values.ti_transp.diff_eff(nrho0:nrho,nion))
-     # ALLOCATE(coretransp.values.vtor_transp.diff_eff(nrho0:nrho,nion))
-     # ALLOCATE(coretransp.values.ne_transp.vconv_eff(nrho0:nrho,3))
-     # ALLOCATE(coretransp.values.te_transp.vconv_eff(nrho0:nrho))
-     # ALLOCATE(coretransp.values.ni_transp.vconv_eff(nrho0:nrho,nion,3))
-     # ALLOCATE(coretransp.values.ti_transp.vconv_eff(nrho0:nrho,nion))
-     # ALLOCATE(coretransp.values.vtor_transp.vconv_eff(nrho0:nrho,nion))
+    coretransp.values[0].sigma = np.empty((nrho0))
 
-     #coretransp.values.ne_transp.flux = 0. 
-     #coretransp.values.te_transp.flux = 0. 
-     #coretransp.values.ni_transp.flux = 0. 
-     #coretransp.values.ti_transp.flux = 0. 
-     #coretransp.values.vtor_transp.flux = 0. 
-     #coretransp.values.ne_transp.diff_eff = 0. 
-     #coretransp.values.te_transp.diff_eff = 0. 
-     #coretransp.values.ni_transp.diff_eff = 0. 
-     #coretransp.values.ti_transp.diff_eff = 0. 
-     #coretransp.values.vtor_transp.diff_eff = 0. 
-     #coretransp.values.ne_transp.vconv_eff = 0. 
-     #coretransp.values.te_transp.vconv_eff = 0. 
-     #coretransp.values.ni_transp.vconv_eff = 0. 
-     #coretransp.values.ti_transp.vconv_eff = 0. 
-     #coretransp.values.vtor_transp.vconv_eff = 0. 
+    coretransp.values[0].rho_tor = np.empty((nrho0))
+    coretransp.values[0].rho_tor_norm = np.empty((nrho0))
 
+    coretransp.values[0].ne_transp.flux = np.zeros((nrho0))
+    coretransp.values[0].te_transp.flux = np.zeros((nrho0))
+    coretransp.values[0].ni_transp.flux = np.zeros((nrho0, nion))
+    coretransp.values[0].ti_transp.flux = np.zeros((nrho0, nion))
+    coretransp.values[0].vtor_transp.flux = np.zeros((nrho0, nion))
+    coretransp.values[0].ne_transp.diff_eff = np.zeros((nrho0, 3)) # TODO: check exact sizes in Fortran
+    coretransp.values[0].te_transp.diff_eff = np.zeros((nrho0))
+    coretransp.values[0].ni_transp.diff_eff = np.zeros((nrho0, nion, 3))
+    coretransp.values[0].ti_transp.diff_eff = np.zeros((nrho0, nion))
+    coretransp.values[0].vtor_transp.diff_eff = np.zeros((nrho0, nion))
+    coretransp.values[0].ne_transp.vconv_eff = np.zeros((nrho0, 3))
+    coretransp.values[0].te_transp.vconv_eff = np.zeros((nrho0))
+    coretransp.values[0].ni_transp.vconv_eff = np.zeros((nrho0, nion, 3))
+    coretransp.values[0].ti_transp.vconv_eff = np.zeros((nrho0, nion))
+    coretransp.values[0].vtor_transp.vconv_eff = np.zeros((nrho0, nion))
+
+    return coretransp
