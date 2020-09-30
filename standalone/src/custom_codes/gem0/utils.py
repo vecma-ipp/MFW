f171a6f1b57aebf4248333472df90c4e62bd4a95
Yehor Yudin <yehor.yudin@ipp.mpg.de>
2020-09-30 19:01:15 +0200

    Fixed identation type at python-gem0 interpolation function:
     - TODO: change signatures according to python function parameters

--- standalone/src/custom_codes/gem0/utils.py
+++ standalone/src/custom_codes/gem0/utils.py
@@ -7,8 +7,9 @@ def l3interp(y_in, x_in, nr_in, y_out, x_out, nr_out): #TODO currently intepolat
     """
 
     #print("> Interpolation, nr_in:{} nr_out:{}".format(nr_in, nr_out))
-    #print('x_in[{}]: {}; x_in[0]: {}'.format(nr_in, x_in[nr_in], x_in[0]))
+    #print('x_in[{}]: {}; x_in[0]: {}'.format(nr_in, x_in[nr_in - 1], x_in[0]))
     #print('x_out: {}'.format(x_out))
+    
 
     if x_in[nr_in - 1] > x_in[0]:
         jstart = 2
@@ -23,21 +24,21 @@ def l3interp(y_in, x_in, nr_in, y_out, x_out, nr_out): #TODO currently intepolat
 
     j1 = jstart
 
-    print('y_out size is {}'.format(y_out.shape))
-    print('the iteration for interpolation is over {}; {}; {}; {}'.format(jstart, jfirst, jlast, jstep))
+    #print('y_out size is {}'.format(y_out.shape))
+    #print('the iteration for interpolation is over {}; {}; {}; {}'.format(jstart, jfirst, jlast, jstep))
 
     for j in range(jfirst, jlast + 1, jstep):
-        print('j:{}'.format(j))
+        #print('j:{}'.format(j))
         x = x_out[j]
         while x >= x_in[j1] and nr_in - 2 > j1 > 1:
             j1 = j1 + jstep
 
-        print('j1:{}'.format(j1))
+        #print('j1:{}'.format(j1))
         j2 = j1 + jstep
         j0 = j1 - jstep
         jm = j1 - 2 * jstep
 
-        print('j2: {}; j1:{}, j0: {}; jm: {}'.format(j2, j1, j0, jm))
+        #print('j2: {}; j1:{}, j0: {}; jm: {}'.format(j2, j1, j0, jm))
 
         # Extrapolate inside out
 
@@ -57,9 +58,9 @@ def l3interp(y_in, x_in, nr_in, y_out, x_out, nr_out): #TODO currently intepolat
         y_out[j] = aintm * y_in[jm] + aint0 * y_in[j0] + aint1 * y_in[j1] + aint2 * y_in[j2]
 
         #print ('y vals: {} {} {} {}'.format(y_in[j0], y_in[j1], y_in[j2], y_in[jm]))
-        print('y_out : {}, res len:{}'.format(y_out[j], len(y_out)))
+        #print('y_out : {}, res len:{}'.format(y_out[j], len(y_out)))
 
-        return y_out
+    return y_out
 
 def l3deriv(y_in, x_in, nr_in, dydx_out, x_out, nr_out):
     """
@@ -110,7 +111,7 @@ def l3deriv(y_in, x_in, nr_in, dydx_out, x_out, nr_out):
 
         dydx_out[j] = aintm * y_in[jm] + aint0 * y_in[j0] + aint1 * y_in[j1] + aint2 * y_in[j2]
 
-        #print ('y vals: {} {} {} {}'.format(y_in[jm], y_in[j0], y_in[j1], y_in[j2]))
+        #print('y vals: {} {} {} {}'.format(y_in[jm], y_in[j0], y_in[j1], y_in[j2]))
         #print('y_out : {}, res len:{}'.format(dydx_out[j], len(dydx_out)))
 
-        return dydx_out
+    return dydx_out
