f9a3b587f4e9709be4e73c7c15b72fdad4ecc43a
Jalal Lakhlili <jlakhlili@gmail.com>
2021-02-01 17:07:56 +0100

    add prints

--- uq/tests/gem.py
+++ uq/tests/gem.py
@@ -152,6 +152,22 @@ my_campaign.apply_analysis(analysis)
 results = my_campaign.get_last_analysis()
 
 # Get Descriptive Statistics
+mean_el = results.describe('te_transp.flux', 'mean')
+std_el = results.describe('te_transp.flux', 'std')
+mean_io = results.describe('ti_transp.flux', 'mean')
+std_io = results.describe('ti_transp.flux', 'std')
 
+s1_el = results.sobols_first('te_transp.flux')
+s1_io = results.sobols_first('ti_transp.flux')
+
+print("TE TRANSP FLUX")
+print("Mean: ", mean_el)
+print("Std: ", std_el)
+print("Sob1: ", s1_el)
+
+print("TI TRANSP FLUX")
+print("Mean: ", mean_io)
+print("Std: ", std_io)
+print("Sob1: ", s1_io)
 
 print('>>> TEST GEM-UQ: END')
