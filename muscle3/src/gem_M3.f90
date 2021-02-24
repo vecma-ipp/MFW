830b3fe3518e55222f0219d9236773cc7dad4fd9
onnieluk <oluk0000@r000u08l03.marconi.cineca.it>
2021-02-24 16:02:49 +0100

    add GEM-related workflow scripts and modify GEM_M3

--- muscle3/src/gem_M3.f90
+++ muscle3/src/gem_M3.f90
@@ -2,7 +2,7 @@ program gem_M3
   use gem_standalone
   use imp4dv_standalone
   use ymmsl
-  use libmuscle
+  use libmuscle_mpi
   use mpi
   implicit none
 
@@ -16,8 +16,10 @@ program gem_M3
   type(LIBMUSCLE_Message) :: smsg
   type(LIBMUSCLE_Data) :: sdata
 
+  integer, parameter :: root_rank = 0
   integer :: err_code, ierr, npes, irank, ios
   character(:), allocatable :: err_msg
+  integer :: equil_buf_size, coreprof_buf_size
 
   real (selected_real_kind(15)) :: t_cur !, t_max, dt, k
 
@@ -35,7 +37,7 @@ program gem_M3
   call LIBMUSCLE_PortsDescription_add(ports, YMMSL_Operator_F_INIT, 'equilibrium_in')
   call LIBMUSCLE_PortsDescription_add(ports, YMMSL_Operator_F_INIT, 'coreprof_in')
   call LIBMUSCLE_PortsDescription_add(ports, YMMSL_Operator_O_F, 'coretransp_out')
-  instance = LIBMUSCLE_Instance_create(ports)
+  instance = LIBMUSCLE_Instance_create(ports, MPI_COMM_WORLD, root_rank)
   call LIBMUSCLE_PortsDescription_free(ports)
   
   ! main loop
@@ -43,21 +45,44 @@ program gem_M3
 
      ! receive equilibrium data
      rmsg = LIBMUSCLE_Instance_receive(instance, 'equilibrium_in')
-     rdata = LIBMUSCLE_Message_get_data(rmsg)
-     allocate (equilibrium_in_buf(LIBMUSCLE_DataConstRef_size(rdata)))
-     call LIBMUSCLE_DataConstRef_as_byte_array(rdata, equilibrium_in_buf)
-     call LIBMUSCLE_DataConstRef_free(rdata)
-     call LIBMUSCLE_Message_free(rmsg)
+     if (irank == root_rank) then
+        rdata = LIBMUSCLE_Message_get_data(rmsg)
+        equil_buf_size = LIBMUSCLE_DataConstRef_size(rdata)
+        allocate (equilibrium_in_buf(equil_buf_size))
+        call LIBMUSCLE_DataConstRef_as_byte_array(rdata, equilibrium_in_buf)
+        call LIBMUSCLE_DataConstRef_free(rdata)
+        call LIBMUSCLE_Message_free(rmsg)
+     end if
+
+     call MPI_Bcast(equil_buf_size, 1, MPI_INT, root_rank, MPI_COMM_WORLD, ierr)
+
+     if (irank /= root_rank) then
+        allocate (equilibrium_in_buf(equil_buf_size))
+     end if
+
+     call MPI_Bcast(equilibrium_in_buf, equil_buf_size, MPI_BYTE, root_rank, MPI_COMM_WORLD, ierr)
 
      ! receive coreprof data
      rmsg = LIBMUSCLE_Instance_receive(instance, 'coreprof_in')
-     rdata = LIBMUSCLE_Message_get_data(rmsg)
-     allocate (coreprof_in_buf(LIBMUSCLE_DataConstRef_size(rdata)))
-     call LIBMUSCLE_DataConstRef_as_byte_array(rdata, coreprof_in_buf)
-     call LIBMUSCLE_DataConstRef_free(rdata)
-     t_cur = LIBMUSCLE_Message_timestamp(rmsg)
-     !t_max = LIBMUSCLE_Message_timestamp(rmsg) + t_max
-     call LIBMUSCLE_Message_free(rmsg)
+     if (irank == root_rank) then
+        rdata = LIBMUSCLE_Message_get_data(rmsg)
+        coreprof_buf_size = LIBMUSCLE_DataConstRef_size(rdata)
+        allocate (coreprof_in_buf(coreprof_buf_size))
+        call LIBMUSCLE_DataConstRef_as_byte_array(rdata, coreprof_in_buf)
+        call LIBMUSCLE_DataConstRef_free(rdata)
+        t_cur = LIBMUSCLE_Message_timestamp(rmsg)
+        call LIBMUSCLE_Message_free(rmsg)
+     end if
+
+     call MPI_Bcast(coreprof_buf_size, 1, MPI_INT, root_rank, MPI_COMM_WORLD, ierr)
+
+     if (irank /= root_rank) then
+        allocate (coreprof_in_buf(coreprof_buf_size))
+     end if
+
+     call MPI_Bcast(coreprof_in_buf, coreprof_buf_size, MPI_BYTE, root_rank, MPI_COMM_WORLD, ierr)
+
+     call MPI_Barrier(MPI_COMM_WORLD, ierr)
 
      call gem2buf( &
           equilibrium_in_buf, &
