1c364e3d33c34c8e5320c3f07e96e1c937895656
Yehor Yudin <yehor.yudin@ipp.mpg.de>
2021-09-10 15:48:06 +0200

    Added a local bash file to plot GEM values

---
+++ uq/basicda/use_gem_da.sh
@@ -0,0 +1,17 @@
+
+NUM=4
+NUMPR=$(($NUM-1))
+CPONUM=$(($NUM+13))
+
+python3 gem_da.py $CPONUM
+
+cp 'GEM_plots/gem_ti_transp_flux_evol_all'$NUMPR'.csv' ./
+cat 'gem_ti_transp_flux_evol_all'$NUMPR'.csv' 'gem_ti_transp_evol_'$CPONUM'.csv' > 'gem_ti_transp_flux_evol_all'$NUM'.csv'
+
+python3 gem_da.py 'all'$NUM
+
+mv *.txt GEM_plots/
+mv *.csv GEM_plots/
+mv *.png GEM_plots/
+
+
