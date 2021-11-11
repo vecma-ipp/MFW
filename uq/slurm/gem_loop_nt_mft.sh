855941fe5626bb0602a30cc88a2644a5ad432b82
Yehor Yudin <yehor.yudin@ipp.mpg.de>
2021-11-11 19:35:53 +0100

    Some fixes in slurm shell scripts for GEM and EasyVVUQ

---
+++ uq/slurm/gem_loop_nt_mft.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -l
+
+#SBATCH --job-name=gem_loop_nt_mft
+
+#SBATCH --output=gem-loop-out.%j
+#SBATCH --error=gem-loop-err.%j
+
+#SBATCH --time=24:00:00
+
+#SBATCH --nodes=2
+#SBATCH --tasks-per-node=48
+
+#SBATCH --partition=skl_fua_prod
+
+#SBATCH --account=FUA35_UQMWA3
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=yyudin@ipp.mpg.de
+
+export SYS=MARCONI
+export SCRATCH=$CINECA_SCRATCH
+export MPICMD=srun
+export LD_LIBRARY_PATH=${FFTW_HOME}/lib:${LD_LIBRARY_PATH}
+
+ENCODER_MODULES="mfw.templates.cpo_encoder;mfw.templates.xml_encoder"
+export ENCODER_MODULES
+export EASYPJ_CONFIG=conf.sh
+
+#cd $SYS
+#rm loop_gem_notransp
+#cd ../../..
+#make standalone
+#cd standalone/bin
+
+echo '> Running 8ft original AUG case with 50 internasl tst; using t0*.dat files after run6 of 2988 calls'
+
+srun -n 64 -N 2 MARCONI/loop_gem_notransp > gem-loop-log.${SLURM_JOBID}
+
