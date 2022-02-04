7f5399254101269fdc9616a5d0e6e5c94bdc9447
Yehor Yudin <yehor.yudin@ipp.mpg.de>
2022-02-04 16:24:14 +0100

    Now SLURM workflow bash script has outputs, accepts parameters and is commented

--- uq/continuous_gem.sh
+++ uq/continuous_gem.sh
@@ -4,6 +4,7 @@
 # implemented as chaining of SLURM submissions
 
 #0. State the total number of campaigns to run, and ordinal number of the last campaign in previous sequence
+echo "STARTING THE WORKFLOW"
 # number of runs
 NUMRUNS=3 
 # no of last/current run
@@ -17,25 +18,39 @@ LASTRUN=$((${CURRUN}+${NUMRUNS}))
 COM=run_marconi_loop_resume_gem_nt.sh
 #TODO: reduce submision time in $COM SLURM script and number of code calls in GEM iterator
 
+echo "Before first submission, here are the numbers"
+echo "Total number of new runs: "$NUMRUNS
+echo "Last completed run number: "$CURRUN
+echo "First new run number: "$FRUN
+echo "Number of last run in this submission: "$LASTRUN
+
 #1. First submission of a campaign, retrieve the SLURM job-id
+echo "Starting the very first SLURM submission with UQ campaign"
 PREVID=$(sbatch --wait ${COM})
 #1'. Save and process the outputs after the finish of the submission
+echo "Finished first new UQ campaign, now postprocessing"
 cd basicda
 ./gem_postproc_vary_test.sh ${FRUN}
 cd ..
 FRUN=$((${FRUN}+1))
+echo "Finished prostprocessing, next run number: "$FRUN
 
 #2. Loop over the rest of the campaign submissions
 for n in `seq ${FRUN} ${LASTRUN}`; do
     #TODO: make while loop, or rather do-until loop
+    
+    echo "Starting next submission, no "${n} 
     CURID=$(sbatch --dependency=afterany:${PREVID} --wait ${COM})
     #TODO: ideally use =afterok and make sure there are no errors in the UQ script
     #TODO make sure that the output files have either continius numerations, or stored separately
     
+    echo "Finished UQ campaign, starting postprocessing"
     cd basicda
     ./gem_postproc_vary_test.sh ${n}
     cd ..
 
     PREVID=$CURID
+    echo "Finished postprocessing"
 done
 
+echo "FINISHED THE WORKFLOW"
