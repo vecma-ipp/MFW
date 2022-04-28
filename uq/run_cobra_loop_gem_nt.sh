29515f72dad9e2e7b8dca2d0f7c67130155114f3
Yehor Yudin <yehor.yudin@ipp.mpg.de>
2022-04-28 18:37:20 +0200

    Additional flag for QCG-PJ

--- uq/run_cobra_loop_gem_nt.sh
+++ uq/run_cobra_loop_gem_nt.sh
@@ -11,9 +11,9 @@
 #SBATCH --time=23:30:00
 
 ## number of nodes and tasks per node
-# order=3, n_params=4, n_subd=8 -> 1024 across 40(80?) cpn -> 27 nodes
+# order=3, n_params=4, n_subd=8 -> 1024 across 40 (80 for hthreading, not used) cpus -> 27 nodes
 ###SBATCH --nodes=27 # MIND number of parameters in variation in the script
-#SBATCH --nodes=4
+#SBATCH --nodes=5
 #SBATCH --ntasks-per-node=40
 ###SBATCH --ntasks-per-core=1
 ###SBATCH --cpus-per-task=8
@@ -26,9 +26,13 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=yyudin@ipp.mpg.de
 
+######################################
+# Loading modules
 module load anaconda/3/2021.11 intel/21.5.0 impi/2021.5 mkl/2020.1 fftw-mpi/3.3.10
 
-conda activate python394
+# Python set-up
+#conda activate python394
+source activate $HOME/conda-envs/python394
 
 export SYS=COBRA
 export SCRATCH=$SCRATCH
@@ -36,7 +40,8 @@ export SCRATCH=$SCRATCH
 #export PYTHONPATH=/u/yyudin/codes/ual_python_interface:$PYTHONPATH
 #export PYTHONPATH=/u/yyudin/codes/MFW/uq:$PYTHONPATH
 
-export MPICMD=mpiexec #srun #mpirun #intelmpi
+# MPI programs starter, MPCDF recommens using 'srun' only at COBRA!
+export MPICMD=srun #mpirun #mpiexec #intelmpi
 export LD_LIBRARY_PATH=${FFTW_HOME}/lib:${LD_LIBRARY_PATH}
 
 # For QCG-PilotJob usage
@@ -44,11 +49,17 @@ ENCODER_MODULES="mfw.templates.cpo_encoder;mfw.templates.xml_encoder"
 export ENCODER_MODULES
 export EASYPJ_CONFIG=conf.sh
 
+# Trying to enable srun 
+export I_MPI_HYDRA_BOOTSTRAP_EXEC_EXTRA_ARGS="--exclusive"
+
 # Define some global variables to configure UQ software
-export MPIMOD=srunmpi #default
+export MPIMOD=default #srunmpi
+
+export NCORESPTASK=8
 
 echo -e '> In this run: use ExecuteLocal only + QCGPJ pool + '$MPIMOD' exec mode + '$SLURM_NNODES' nodes + 4 params + commandline passed with '$MPICMD' /n'
 
+####################################
 # Run the UQ code
 
 # Echo SLURM environmental variables
