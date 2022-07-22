f28f7963c9be9d0a67adfe6e130307bf6032aba5
Yehor Yudin <yehor.yudin@ipp.mpg.de>
2022-07-22 10:56:16 +0200

    Tried allocating whole nodes for independent GEM runs

--- uq/write_run_files.sh
+++ uq/write_run_files.sh
@@ -1,17 +1,27 @@
 #!/bin/bash -l
 
-rundate='21072022'
-runtime='1515'
+rundate='22072022'
+runtime='1045'
 
-jobslurmid='5745295'
-jobcampid='v4b7_ppx'
-nodenums=('3326')
+jobslurmid='5748887'
+jobcampid='s491vudh'
+nodenums=('3566')
+
+### Recording QCG-PJ issues
 
 # first try: 19072022 1100 5733612 3qa2cg2a ('6047' '6049')
 
-# default+srun+flags: 19072022 1800 5736377 4e92_3xx ('6035')
-# default+srun+no_flags: 21072022 1500 5745061 17bf_svp ('4521')
-# srunmpi+exec+no_flags: 21072022 1515 5745295 v4b7_ppx ('3326')
+# default+srun+flags: 19072022 1800 5736377 4e92_3xx ('6035')  -> SLURM halt
+
+# srunmpi+exec+no_flags: 21072022 1500 5745061 17bf_svp ('4521') > MPI_Init erro
+# default+srun+no_flags: 21072022 1515 5745295 v4b7_ppx ('3326') -> SLURM halt
+# srunmpi+exec+no_flags: 21072022 1600 5745650 kzpli7gh ('4321') -> SLURM halt
+
+# srunmpi+srun+no_flag: 21972022 1630 5745709 flwkdt2q ('4371') -> MPI_Init error
+# default+srun+no_flags: 21972022 1645 5745754 ue1y9103 ('3570') -> SLURM halt
+
+# default+srun+no_flags+npess32(2 independent nodes): 22072022 1030 5748864 sz6hftfj ('4585' '4590') -> 2/2 codes running!
+# default+srun+no_flags+16(1 node): 22072022 1045 5748887 s491vudh ('3566') -> SLURM disables creation
 
 ### Make directory - copy - archivize
 
@@ -43,6 +53,7 @@ cp /ptmp/yyudin/VARY_1FT_GEM_NT_${jobcampid}/.qcgpj_in_prev_* run_${rundate}_${r
 cp /ptmp/yyudin/VARY_1FT_GEM_NT_${jobcampid}/stdout_* run_${rundate}_${runtime}/scratch
 cp /ptmp/yyudin/VARY_1FT_GEM_NT_${jobcampid}/stderr_* run_${rundate}_${runtime}/scratch
 cp /ptmp/yyudin/VARY_1FT_GEM_NT_${jobcampid}/campaign.db run_${rundate}_${runtime}/scratch
+cp -r /ptmp/yyudin/VARY_1FT_GEM_NT_${jobcampid}/common/ run_${rundate}_${runtime}/scratch
 
 # The archive
 tar -czvf run_${rundate}_${runtime}.tar.gz run_${rundate}_${runtime}
