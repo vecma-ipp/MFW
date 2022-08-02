dbf1b73443260ea0b85c16a6bb42709dfacb144b
Yehor Yudin <yehor.yudin@ipp.mpg.de>
2022-08-02 19:22:31 +0200

    Fix in bash postprocessing script for files

--- uq/basicda/sort_files.sh
+++ uq/basicda/sort_files.sh
@@ -3,16 +3,24 @@
 cd gem_data/
 
 qs=('te' 'ti' 'ne' 'ni')
-ts=('gem' 'resuq' 'stats' 'fft' 'scan')
-ns=5
+ts=('gem' 'resuq' 'stats' 'fft' 'scan' 'pdf' 'hist')
+ns=1
 
 for q in ${qs[@]} ; do
+
+  mkdir ${q}
+
   for t in ${ts[@]} ; do	  
+   
     mkdir ${q}/${t}/
-    mv ${q}/${t}_* ${q}/${t}/
+    #mv ${q}/${t}_* ${q}/${t}/
+   
     for n in `seq 1 ${ns}`; do
+   
       mkdir ${q}/${t}/${n}/
-      mv ${q}/${t}/*_${n}* ${q}/${t}/${n}/
+    
+      mv ${t}*${q}*_${n}* ${q}/${t}/${n}/
+   
     done
   done
 done
