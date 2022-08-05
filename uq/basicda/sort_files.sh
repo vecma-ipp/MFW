f3b1a9fae48f51bccacdd64674ee7afa1e04e721
Yehor Yudin <yehor.yudin@ipp.mpg.de>
2022-08-05 15:00:42 +0200

    Sort files of postprocessign script - update

--- uq/basicda/sort_files.sh
+++ uq/basicda/sort_files.sh
@@ -2,17 +2,24 @@
 
 cd gem_data/
 
+# Put ouputs after GEM postprocessing into quantity/plot/runnum folder structure
+
 qs=('te' 'ti' 'ne' 'ni')
 ts=('gem' 'resuq' 'stats' 'fft' 'scan')
 ns=5
 
 for q in ${qs[@]} ; do
+
   for t in ${ts[@]} ; do	  
+
     mkdir ${q}/${t}/
     mv ${q}/${t}_* ${q}/${t}/
+
     for n in `seq 1 ${ns}`; do
+
       mkdir ${q}/${t}/${n}/
       mv ${q}/${t}/*_${n}* ${q}/${t}/${n}/
+
     done
   done
 done
