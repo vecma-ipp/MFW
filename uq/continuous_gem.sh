d499e76847c2c12a4490172f65f662de7a27b077
Yehor Yudin <yehor.yudin@ipp.mpg.de>
2022-09-26 15:56:13 +0200

    Now UQ WF for many SLURM submissions considers a tree of run directories and golbal polynomial order value

--- uq/continuous_gem.sh
+++ uq/continuous_gem.sh
@@ -5,11 +5,12 @@
 
 # Launch with:
 # nohup ./continuous_gem.sh 17 aos1mzke > script_workflow_latest3.log 2>&1 &
+# nohup ./continuous_gem.sh > script_wf_20092022.log 2>&1 &
 
 #0. State the total number of campaigns to run, and ordinal number of the last campaign in previous sequence
 echo "STARTING THE WORKFLOW"
 # number of runs
-NUMRUNS=12
+NUMRUNS=5
 # no of current run, which is the last finished submission
 CURRUN=${1:-0}
 # no of the first run in the new sequence
@@ -17,18 +18,25 @@ FRUN=$((${CURRUN}+1))
 # no of the last run in the new sequence
 LASTRUN=$((${CURRUN}+${NUMRUNS}))
 
+# polynomial order - current parameter regulating total number of code instances
+POLORDER = 2
+INPUT_DIM = 4
+
 # batch script to submit a single UQ campaign
 #COM=run_marconi_loop_resume_gem_nt.sh # for MARCONI
 COM=run_cobra_loop_resume_gem_nt.sh # for COBRA
 COM0=run_cobra_loop_gem_nt.sh
 
 RUNRANGESTART=1
-RUNRANGE=4
+#RUNRANGE=16
+RUNRANGE=$((${POLORDER} ** ${INPUT_DIM}))
 
 CPONUM=${FRUN}
 
 echo "Before first submission, here are the numbers"
 echo "Total number of new runs: "${NUMRUNS}
+
+export ORIGDIR=$(pwd)
 
 if [ "${CURRUN}" -gt 0 ];
 then 
@@ -46,33 +54,41 @@ then
 
   # 0.0. Backing up snapshot files from the last runs
 
+  BCKP_FILES = ('gem_coretransp*.cpo' '*.dat' 'fout_0*' 'stopped')
+
   TMP=${RANDOM}
-  RUNPATHSHORT=runs/runs_0-100000000/runs_0-1000000/runs_0-10000/runs_0-100
+  #RUNPATHSHORT=runs/runs_0-100000000/runs_0-1000000/runs_0-10000/runs_0-100
+  RUNPATHSHORT=runs/
 
-  mkdir ${SCRATCH}/VARY_1FT_GEM_NT_${ROOTCAMPDIR}/${RUNPATHSHORT}/bckp/
-  mkdir ${SCRATCH}/VARY_1FT_GEM_NT_${ROOTCAMPDIR}/${RUNPATHSHORT}/bckp/${TMP}
+  #mkdir ${SCRATCH}/VARY_1FT_GEM_NT_${ROOTCAMPDIR}/${RUNPATHSHORT}/bckp/
+  mkdir -p ${SCRATCH}/VARY_1FT_GEM_NT_${ROOTCAMPDIR}/${RUNPATHSHORT}/bckp/${TMP}
 
-  for r in `seq ${RUNRANGESTART} ${RUNRANGE}`; do
-    mkdir ${SCRATCH}/VARY_1FT_GEM_NT_${ROOTCAMPDIR}/${RUNPATHSHORT}/bckp/${TMP}/run_${r}/
+  cd ${SCRATCH}/VARY_1FT_GEM_NT_${ROOTCAMPDIR}/${RUNPATHSHORT}/
 
-    cp ${SCRATCH}/VARY_1FT_GEM_NT_${ROOTCAMPDIR}/${RUNPATHSHORT}/run_${r}/gem_coretransp*.cpo ${SCRATCH}/VARY_1FT_GEM_NT_${ROOTCAMPDIR}/${RUNPATHSHORT}/bckp/${TMP}/run_${r}/
+  #for r in `seq ${RUNRANGESTART} ${RUNRANGE}`; do
+  for r in (find -maxdepth 5 -mindepth 5 -type d -name "run*"); do
 
-    cp ${SCRATCH}/VARY_1FT_GEM_NT_${ROOTCAMPDIR}/${RUNPATHSHORT}/run_${r}/*.dat ${SCRATCH}/VARY_1FT_GEM_NT_${ROOTCAMPDIR}/${RUNPATHSHORT}/bckp/${TMP}/run_${r}/
+    #mkdir ${SCRATCH}/VARY_1FT_GEM_NT_${ROOTCAMPDIR}/${RUNPATHSHORT}/bckp/${TMP}/run_${r}/
+    mkdir -p bckp/${TMP}/${r}/
 
-    cp ${SCRATCH}/VARY_1FT_GEM_NT_${ROOTCAMPDIR}/${RUNPATHSHORT}/run_${r}/fout_0* ${SCRATCH}/VARY_1FT_GEM_NT_${ROOTCAMPDIR}/${RUNPATHSHORT}/bckp/${TMP}/run_${r}/
+    for bckp_f in ${BCKP_FILES[@]} ; do 
 
-    cp ${SCRATCH}/VARY_1FT_GEM_NT_${ROOTCAMPDIR}/${RUNPATHSHORT}/run_${r}/stopped ${SCRATCH}/VARY_1FT_GEM_NT_${ROOTCAMPDIR}/${RUNPATHSHORT}/bckp/${TMP}/run_${r}/
+      #cp ${SCRATCH}/VARY_1FT_GEM_NT_${ROOTCAMPDIR}/${RUNPATHSHORT}/run_${r}/${bckp_f} ${SCRATCH}/VARY_1FT_GEM_NT_${ROOTCAMPDIR}/${RUNPATHSHORT}/bckp/${TMP}/run_${r}/
+      cp ${r}/${bckp_f} /bckp/${TMP}/${r}/
+
+    done
 
   done
 
   # 0.1. Restoring snapshot files to the state of desired last completed run
-  for r in `seq ${RUNRANGESTART} ${RUNRANGE}`; do
+  #for r in `seq ${RUNRANGESTART} ${RUNRANGE}`; do
+  for r in (find -maxdepth 5 -mindepth 5 -type d -name "run*"); do
 
-    cp ${SCRATCH}/VARY_1FT_GEM_NT_${ROOTCAMPDIR}/${RUNPATHSHORT}/dat/${CURRUN}/run_${r}/*.dat ${SCRATCH}/VARY_1FT_GEM_NT_${ROOTCAMPDIR}/${RUNPATHSHORT}/run_${r}/
+    #cp ${SCRATCH}/VARY_1FT_GEM_NT_${ROOTCAMPDIR}/${RUNPATHSHORT}/dat/${CURRUN}/run_${r}/*.dat ${SCRATCH}/VARY_1FT_GEM_NT_${ROOTCAMPDIR}/${RUNPATHSHORT}/run_${r}/
+    cp /dat/${CURRUN}/${r}/*.dat ${r}/
 
-    cp ${SCRATCH}/VARY_1FT_GEM_NT_${ROOTCAMPDIR}/${RUNPATHSHORT}/dat/${CURRUN}/run_${r}/fout_0* ${SCRATCH}/VARY_1FT_GEM_NT_${ROOTCAMPDIR}/${RUNPATHSHORT}/run_${r}/
-
-    #cp ${SCRATCH}/VARY_1FT_GEM_NT_${ROOTCAMPDIR}/${RUNPATHSHORT}/dat/${CURRUN}/run_${r}/stopped ${SCRATCH}/VARY_1FT_GEM_NT_${ROOTCAMPDIR}/${RUNPATHSHORT}/run_${r}/
+    #cp ${SCRATCH}/VARY_1FT_GEM_NT_${ROOTCAMPDIR}/${RUNPATHSHORT}/dat/${CURRUN}/run_${r}/fout_0* ${SCRATCH}/VARY_1FT_GEM_NT_${ROOTCAMPDIR}/${RUNPATHSHORT}/run_${r}/
+    cp /dat/${CURRUN}/${r}/fout_0* ${r}/
 
   done
   #cd ${HOME}/code/MFW/uq/
@@ -81,7 +97,7 @@ then
   echo "Starting the very first SLURM submission with UQ campaign"
 
   #PREVID=$(sbatch --parsable --wait ${COM} 2>&1 | sed 's/[S,a-z]* //g')
-  PREVID=$(sbatch --export=ALL,CPONUM=${FRUN},OLDCAMP=${ROOTCAMPDIR} --parsable --wait ${COM})
+  PREVID=$(sbatch --export=ALL,CPONUM=${FRUN},OLDCAMP=${ROOTCAMPDIR},POLORDER=${POLORDER} --parsable --wait ${COM})
 
   #1'. Save and process the outputs after the finish of the submission
   echo "Finished the first new UQ campaign "${PREVID}
@@ -96,7 +112,7 @@ else
   #1. First submission of a campaign, retrieve the SLURM job-id
   echo "Starting the VERY ACTUAL first SLURM submission with UQ campaign"
 
-  PREVID=$(sbatch --export=ALL --parsable --wait ${COM0})
+  PREVID=$(sbatch --export=ALL,POLORDER=${POLORDER} --parsable --wait ${COM0})
 
   #Extract the ROOTCAMPDIR from the submission
   ROOTCAMPDIR=$(<camp_temp_dir.txt) 
@@ -125,7 +141,7 @@ for n in `seq ${FRUN} ${LASTRUN}`; do
 
     CPONUM=${n}
     #CURID=$(sbatch --export=ALL,CPONUM=${n} --parsable --dependency=afterany:${PREVID}:+3 --wait ${COM}  2>&1 | sed 's/[S,a-z]* //g')
-    CURID=$(sbatch --export=ALL,CPONUM=${n},OLDCAMP=${ROOTCAMPDIR} --parsable --dependency=afterany:${PREVID}:+3 --wait ${COM})
+    CURID=$(sbatch --export=ALL,CPONUM=${n},OLDCAMP=${ROOTCAMPDIR},POLORDER=${POLORDER} --parsable --dependency=afterany:${PREVID}:+3 --wait ${COM})
     #TODO: ideally use =afterok and make sure there are no errors in the UQ script
     #TODO make sure that the output files either have continuous numerations or stored separately
     
