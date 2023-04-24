1b5ff9c075e5d7a905fece85a5eabdc3d89ca6df
Yehor Yudin <yehor.yudin@ipp.mpg.de>
2023-04-24 19:21:01 +0200

    Changes in YMMSL configureation for surrogate version of workflow + DEBUGGING

--- muscle3/workflow/gem_surr_workflow.sh
+++ muscle3/workflow/gem_surr_workflow.sh
@@ -10,7 +10,9 @@ echo 'Running gem_surr workflow in Fortran & Python'
 
 #. ~/muscle3_venv/bin/activate
 . ~/muscle3/bin/muscle3.env
-muscle_manager gem-surr-fusion.ymmsl &
+muscle_manager --start-all gem-surr-fusion.ymmsl &
+
+echo 'MUSCLE_MANAGER started'
 
 manager_pid=$!
 
@@ -22,16 +24,18 @@ SRCDIR=../src/
 #TODO compile wrapper implementations
 #TODO make sure MUSCLE3 works with FORTRAN
 
-$BINDIR/stop_M3 --muscle-instance=stop >'stop.log' 2>&1 &
-$BINDIR/duplicate_M3 --muscle-instance=duplicate >'duplicate.log' 2>&1 &
-python3 $SRCDIR/gem_surr_M3.py --muscle-instance=turbulence >'turbulence.log' 2>&1 &
-$BINDIR/chease_M3 --muscle-instance=equilibrium >'equilibrium.log' 2>&1 &
-$BINDIR/init_M3 --muscle-instance=init >'init.log' 2>&1 &
-$BINDIR/ets_M3 --muscle-instance=transport >'transport.log' 2>&1 &
-$BINDIR/imp4dv_M3 --muscle-instance=f2dv >'f2dv.log' 2>&1 &
-$BINDIR/cpdup_M3 --muscle-instance=cpdup >'cpdup.log' 2>&1 &
+# $BINDIR/stop_M3 --muscle-instance=stop >'stop.log' 2>&1 &
+# $BINDIR/duplicate_M3 --muscle-instance=duplicate >'duplicate.log' 2>&1 &
+# python3 $SRCDIR/gem_surr_M3.py --muscle-instance=turbulence >'turbulence.log' 2>&1 &
+# $BINDIR/chease_M3 --muscle-instance=equilibrium >'equilibrium.log' 2>&1 &
+# $BINDIR/init_M3 --muscle-instance=init >'init.log' 2>&1 &
+# $BINDIR/ets_M3 --muscle-instance=transport >'transport.log' 2>&1 &
+# $BINDIR/imp4dv_M3 --muscle-instance=f2dv >'f2dv.log' 2>&1 &
+# $BINDIR/cpdup_M3 --muscle-instance=cpdup >'cpdup.log' 2>&1 &
 
 touch muscle3_manager.log
+
+echo '> Now launching the manager'
 tail -f muscle3_manager.log --pid=${manager_pid}
 
 wait
