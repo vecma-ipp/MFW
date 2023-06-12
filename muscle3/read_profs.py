3a3492813175fee917344937adf3356c33a8c847
Yehor Yudin <yehor.yudin@ipp.mpg.de>
2023-06-12 19:01:24 +0200

    Muscle3 WF: now plots history of profile evolution

--- muscle3/read_profs.py
+++ muscle3/read_profs.py
@@ -6,13 +6,21 @@ from ascii_cpo import read
 sys.path.append('../uq/basicda')
 from gem_da import profile_evol_load, profile_evol_plot
 
-def read_attrib(filename, quantity, attribute, coord, filetype='coreprof'):
+def read_attrib(filename, quantity, attribute, coords, filetype='coreprof'):
+    """
+    Returns list of attribute values, for every given coordinate number
+    """
     cpo_obj = read(filename, filetype)
-    q = getattr(cpo_obj, quantity)
+    q = cpo_obj
+    if filetype == 'coretransp':
+        q = cpo_obj.values[0]
+    q = getattr(q, quantity)
     a = getattr(q, attribute)
-    return a[coord]
+    # ..[0] for ions, if it is the first ion species
+    a_s = [a[c][0] for c in coords]
+    return a_s
 
-def read_files(foldername, quantity, attribute, coord, filetype='coreprof'):
+def read_files(foldername, quantity, attribute, coords, filetype='coreprof', date=''):
     
     file_base_tocheck = filetype+'_'
     file_ext = '.cpo'
@@ -27,47 +35,53 @@ def read_files(foldername, quantity, attribute, coord, filetype='coreprof'):
     attributes = []
 
     for f in file_names:
-        a = read_attrib(foldername+f, quantity, attribute, coord, filetype)
+        a = read_attrib(foldername+f, quantity, attribute, coords, filetype)
+        #print(f"a= {a}") ###DEBUG
         attributes.append(a)
 
+    # List of lists to a numpy array
     atrributes_array = np.array(attributes)
-    np.savetxt('res_'+quantity+'_'+attribute+'.csv', np.array(atrributes_array), delimiter=',')
+    np.savetxt('res_'+quantity+'_'+attribute+'_'+date+'.csv', np.array(atrributes_array), delimiter=',')
     
     return atrributes_array
 
-
-date = '20230607_122233'
-fold_name = 'workflow/run_fusion_gem_surr_'+date+'/instances/transport/workdir/'
+date = '20230612_165511'
+codename = 'gem0'
+fold_name = 'workflow/run_fusion_'+codename+'_'+date+'/instances/transport/workdir/'
 
 n_run = 10
 runnum_list = [r for r in range (n_run)]
 
-name = 'coreprof'
+cpo_name = 'coreprof'
+#cpo_name = 'coretransp'
 
 quantities =  ['ti']
-attributes = ['value']
+#quantities = ['ti_transp']
+attributes = ['ddrho', 'value',]
+#attributes = ['vconv_eff', 'diff_eff']
 
-coord_num = [68]
+#coord_num = [68]
+coord_num = [14, 30, 43, 55, 66, 76, 85, 95]
+#coord_num = [0,1,2,3,4,5,6,7]
 
-data = read_files(fold_name, quantities[0], attributes[0], coord=coord_num[0], filetype='coreprof')
+# Reading data of a particular attribute for all files produced by transport code
+data = read_files(fold_name, quantities[0], attributes[0], coords=coord_num, filetype=cpo_name, date=date)
 #print(f"data shape: {data.shape}") #DEBUG
-n_data = len(data)
+n_timesteps = data.shape[0]
+n_rhos = data.shape[1]
 
+# Plotting an attribute value at a the fourth flux tube against time
+n_ft = 4
 fig, ax = plt.subplots()
-ax.plot(np.linspace(0, n_data, n_data), data)
+ax.plot(np.linspace(0, n_timesteps, n_timesteps), data[:, n_ft])
 fig.savefig('res_'+quantities[0]+'_'+attributes[0]+'_'+date+'.png')
+plt.close()
 
-# val_ev_s, file_names = profile_evol_load(prof_names=quantities, 
-#                                         attrib_names=attributes, 
-#                                         coord_len=len(coord_num), 
-#                                         folder_name=fold_name, 
-#                                         file_code_name=name, 
-#                                         name_postfix='',
-#                                         file_base_intermediate='',
-#                                         )
-
-# profile_evol_plot(val_ev_s, 
-#                   labels=['ti_value'], 
-#                   name=name+'_'+quantities[0]+'_'+attributes[0], 
-#                   alignment='start', 
-#                   vertline=False) 
+# Plotting attribute profile for multiple time-steps
+n_timesteps_toplot = 10
+fig, ax = plt.subplots()
+for t in range(1, n_timesteps, n_timesteps//n_timesteps_toplot):
+    ax.plot(np.array(coord_num), data[t, :], label=f"t={t}", marker='.')
+ax.legend(loc='best')
+fig.savefig('prof_'+quantities[0]+'_'+attributes[0]+'_'+date+'.png')
+plt.close()
