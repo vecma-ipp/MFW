3b6d627f909de825cead67e57d41a469cb45b472
Yehor Yudin <yehor.yudin@ipp.mpg.de>
2023-06-13 17:57:04 +0200

    MUSCLE3 WF: implements a version with GEM

--- muscle3/workflow/cobra-M3.sl
+++ muscle3/workflow/cobra-M3.sl
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --time=1:00:00         	# time limits
+#SBATCH --time=02:00:00         	# time limits
 #SBATCH --nodes=1 			# nodes
 #SBATCH --ntasks-per-node=40		# tasks per node
 ###SBATCH --cpus-per-task=1		# CPU per task
 #SBATCH --partition=medium	# partition to be used
 ###SBATCH --qos=			# quality of service
-#SBATCH --job-name=gem_sur_M3		# job name
+#SBATCH --job-name=gem_M3		# job name
 #SBATCH --err=test-%j.err			# std-error file
 #SBATCH --out=test-%j.out			# std-output file
 ###SBATCH --account= 			# account number
@@ -14,8 +14,6 @@
 
 cd $PWD
 
-#TODO: may be start with no MPI version
-
 #module purge
 #module use -p $WORK/MODULES
 #module load mfw/gnu-6.1.0
@@ -23,11 +21,10 @@ export SYS=COBRA
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/cobra/u/yyudin/muscle3/lib
 
-
 #echo "GEM cpu parameters:"
 #grep -A4 "npesx" gem.xml |grep "<n"
 
 echo "STARTING MUSCLE3 FROM $PWD"
 #./gem0_workflow.sh	# gem0 workflow (no MPI)
 
-MUSCLE3_HOME=/u/yyudin/muscle3 ./gem_sur_workflow.sh
+MUSCLE3_HOME=/u/yyudin/muscle3 ./gem_workflow.sh
--- muscle3/workflow/gem_workflow.sh
+++ muscle3/workflow/gem_workflow.sh
@@ -8,24 +8,30 @@ fi
 
 echo 'Running gem workflow in Fortran'
 
-. ~/muscle3_venv/bin/activate
-muscle_manager gem-fusion.ymmsl &
+#. ~/muscle3_venv/bin/activate
+. ~/muscle3/bin/muscle3.env
+muscle_manager --start-all gem-fusion-m3.ymmsl &
+
+echo 'MUSCLE_MANAGER started'
 
 manager_pid=$!
 
 #export LD_LIBRARY_PATH=$MUSCLE3_HOME/lib:$LD_LIBRARY_PATH
-BINDIR=../bin/MARCONI-GNU
+#BINDIR=../bin/MARCONI-GNU
+BINDIR=../bin/COBRA/
+SRCDIR=../src/
 
-$BINDIR/stop_M3 --muscle-instance=stop >'stop.log' 2>&1 &
-$BINDIR/duplicate_M3 --muscle-instance=duplicate >'duplicate.log' 2>&1 &
-mpirun -n 8 ./$BINDIR/gem_M3 --muscle-instance=turbulence >'turbulence.log' 2>&1 &
-$BINDIR/chease_M3 --muscle-instance=equilibrium >'equilibrium.log' 2>&1 &
-$BINDIR/init_M3 --muscle-instance=init >'init.log' 2>&1 &
-$BINDIR/ets_M3 --muscle-instance=transport >'transport.log' 2>&1 &
-$BINDIR/imp4dv_M3 --muscle-instance=f2dv >'f2dv.log' 2>&1 &
-$BINDIR/cpdup_M3 --muscle-instance=cpdup >'cpdup.log' 2>&1 &
+# $BINDIR/stop_M3 --muscle-instance=stop >'stop.log' 2>&1 &
+# $BINDIR/duplicate_M3 --muscle-instance=duplicate >'duplicate.log' 2>&1 &
+# mpirun -n 8 ./$BINDIR/gem_M3 --muscle-instance=turbulence >'turbulence.log' 2>&1 &
+# $BINDIR/chease_M3 --muscle-instance=equilibrium >'equilibrium.log' 2>&1 &
+# $BINDIR/init_M3 --muscle-instance=init >'init.log' 2>&1 &
+# $BINDIR/ets_M3 --muscle-instance=transport >'transport.log' 2>&1 &
+# $BINDIR/imp4dv_M3 --muscle-instance=f2dv >'f2dv.log' 2>&1 &
+# $BINDIR/cpdup_M3 --muscle-instance=cpdup >'cpdup.log' 2>&1 &
 
 touch muscle3_manager.log
+echo '> Now launching the manager'
 tail -f muscle3_manager.log --pid=${manager_pid}
 
 wait
