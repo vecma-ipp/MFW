9ff62e41a717c6d8d0f722dc150bfa582987b9df
Yehor Yudin <yehor.yudin@ipp.mpg.de>
2023-08-23 17:52:54 +0200

    M3-WF: changed submission script to define folder for data to resume runs with GEM

--- muscle3/workflow/gem_workflow.sh
+++ muscle3/workflow/gem_workflow.sh
@@ -12,8 +12,10 @@ echo 'Running gem workflow in Fortran'
 . ~/muscle3/bin/muscle3.env
 #source  ~/muscle3/bin/muscle3.env
 
+currdate=$(date +"%Y%m%d")
+
 #muscle_manager --start-all gem-fusion-m3.ymmsl &
-muscle_manager --log-level DEBUG --start-all gem-fusion-m3.ymmsl &
+muscle_manager --log-level DEBUG --run-dir run_fusion_gem_${currdate} --start-all gem-fusion-m3.ymmsl &
 
 echo 'MUSCLE_MANAGER started'
 
@@ -37,7 +39,7 @@ wait
 # Copy the results in a folder for the next submission and continuation of the run 
 # (ALTERNATIVELY: pass the folder of the current finished run)
 # TODO: get the last run folder from within the script
-folder_to_copy_from=run_fusion_gem_20230821_161005
+folder_to_copy_from=run_fusion_gem_${currdate}
 folder_to_copy_to=gem_resume_data
 files_to_copy=('instances/transport/workdir/*_ets_equilibrium_in.cpo' 'instances/transport/workdir/*_ets_coreprof_in.cpo' 'instances/turbulence/workdir/*.dat')
 files_destination=('ets_equilibrium_in.cpo' 'ets_coreprof_in.cpo')
