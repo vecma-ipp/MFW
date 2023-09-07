84e2a79904853791f97cd04747912658c9ac1a5d
Yehor Yudin <yehor.yudin@ipp.mpg.de>
2023-09-07 17:47:12 +0200

    script to run 6 flux tube GEM standalone

--- uq/slurm/gem_loop_nt_8ft.sh
+++ uq/slurm/gem_loop_nt_8ft.sh
@@ -5,7 +5,7 @@
 #SBATCH --output=gem-loop-out.%j
 #SBATCH --error=gem-loop-err.%j
 
-#SBATCH --time=12:00:00
+#SBATCH --time=2:00:00
 
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=40
@@ -26,12 +26,8 @@ ENCODER_MODULES="mfw.templates.cpo_encoder;mfw.templates.xml_encoder"
 export ENCODER_MODULES
 export EASYPJ_CONFIG=conf.sh
 
-#cd $SYS
-#rm loop_gem_notransp
-#cd ../../..
-#make standalone
-#cd standalone/bin
 
 echo '> Running 8ft original AUG case with 50 internal time steps'
 
-${MPICMD} -n 32 -N 1 loop_gem_notransp > gem-loop-log.${SLURM_JOBID}
+${MPICMD} -n 32 -N 1 gem_test_loop > gem-loop-log.${SLURM_JOBID}
+
