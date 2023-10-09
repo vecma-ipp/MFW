5626b2d0b8ab997b3ea324379819026c97ed945e
Yehor Yudin <yehor.yudin@ipp.mpg.de>
2023-10-09 20:12:10 +0200

    M3-WF: postprocessing into single CSV file

--- muscle3/read_profs.py
+++ muscle3/read_profs.py
@@ -123,6 +123,134 @@ def read_equil(foldernames):
     fig.savefig(foldername[0]+'_equilibrium.pdf')
     plt.close()
 
+def plot_quantities(datadict, save_fold_name, coord_num_fts=[14,30,43,55,66,76,85,95], bool_sur_involved=False, n_run_per_ft=81, ref_data=None,):
+    """
+    Saves plots for a tensor square of set of all quantities read during simulation
+    If bool_sur_involved, then ref_data should be passed to add lines of bounds of quantities extrema values occuring in a different simualtions
+    """
+    
+    # Iterate over all pairs of quantity names
+    for q_x,q_y in product(datadict.keys(), datadict.keys()):
+
+        #print(f"- datadict shapes: {datadict[q_x].shape} {datadict[q_y].shape}") ###DEBUG
+
+        #n_fts = min(datadict[q_x].shape[-1], datadict[q_y].shape[-1])
+        n_fts = 8 # extract that's it 8 flux tube from 100 rho coordinates :)
+
+        # Iterate over all flux tubes
+        for n_ft in range(n_fts):
+        
+            fig, ax = plt.subplots()
+
+            t_min = min(len(datadict[q_x][:,n_ft]), len(datadict[q_y][:,n_ft]))
+
+            if 'transp' in q_x:
+                coord_x = n_ft
+            else:
+                coord_x = coord_num_fts[n_ft]
+            
+            if 'transp' in q_y:
+                coord_y = n_ft
+            else:
+                coord_y = coord_num_fts[n_ft]
+
+            ax.plot(datadict[q_x][:t_min,coord_x], 
+                    datadict[q_y][:t_min,coord_y],
+                    marker='.',
+                    )
+
+            # Add vertical and horisontal lines from a reference dataset
+            if bool_sur_involved:
+
+                if q_y in ref_data.columns:
+                    y_min_val = ref_data[q_y].iloc[n_run_per_ft*(n_ft):n_run_per_ft*(n_ft+1)].min()
+                    y_max_val = ref_data[q_y].iloc[n_run_per_ft*(n_ft):n_run_per_ft*(n_ft+1)].max()
+                    ax.hlines(y=y_min_val, xmin=datadict[q_x][:,coord_x].min(), xmax=datadict[q_x][:,coord_x].max(), 
+                            color='r', linestyle='--', label='bounds of the training dataset')
+                    ax.hlines(y=y_max_val, xmin=datadict[q_x][:,coord_x].min(), xmax=datadict[q_x][:,coord_x].max(),
+                            color='r', linestyle='--')
+                
+                if q_x in ref_data.columns:
+                    x_min_val = ref_data[q_x].iloc[n_run_per_ft*(n_ft):n_run_per_ft*(n_ft+1)].min()
+                    x_max_val = ref_data[q_x].iloc[n_run_per_ft*(n_ft):n_run_per_ft*(n_ft+1)].max()
+                
+                    ax.vlines(x=x_min_val, ymin=datadict[q_y][:,coord_y].min(), ymax=datadict[q_y][:,coord_y].max(), 
+                            color='r', linestyle='--',)
+                    ax.vlines(x=x_max_val, ymin=datadict[q_y][:,coord_y].min(), ymax=datadict[q_y][:,coord_y].max(),
+                            color='r', linestyle='--')
+
+            ax.set_xlabel(q_x)
+            ax.set_ylabel(q_y)
+            fig.savefig(save_fold_name+'quant_'+codename+'_' +
+                        q_x+'_'+q_y+'_'+'ft'+str(n_ft)+'_'+dates[0]+'_'+dates[-1]+'.pdf')
+            plt.close()
+    
+    return 0
+
+def write_table_csv(datadict, save_fold_name, coord_num_fts=[14,30,43,55,66,76,85,95]):
+    """
+    Writes a table to a CSV file, where columns are quantities at a particular flux tube and row are time readings
+    """
+
+    n_fts = 8
+    
+    # dictionary of correspondance of local quantity naming to the one used by Onnie Luk
+    dict_naming_dict ={
+        "te_transp_flux": "flux-Te-ft",
+        "ti_transp_flux": "flux-Te-ft",
+        "te_transp_diff_eff": "diff-Te-ft",
+        "ti_transp_diff_eff": "diff-Ti-ft",
+        "te_transp_vconv_eff": "vconv-Te-ft",
+        "ti_transp_vconv_eff": "vconv-Ti-ft",  
+        "te_value": "Te-ft",
+        "ti_value": "Ti-ft",
+        "te_ddrho": "dTe-ft",
+        "ti_ddrho": "dTi-ft",
+        "ne_transp_flux": "flux-ne-ft",
+        "ni_transp_flux": "flux-ni-ft",
+        "ne_value": "ne-ft",
+        "ni_value": "ni-ft",
+        "ne_ddrho": "dne-ft",
+        "ni_ddrho": "dni-ft",
+    }
+
+    t_max = max([v.shape[0] for v in datadict.values()])
+
+    #datadict_table = {k+'_'+str(i%len(coord_num_fts)):vi for vi in v.T for k,v in datadict.items()}
+
+    data_table_dict ={}
+    data_table = np.zeros((t_max, len(datadict.keys())*n_fts))
+
+    # Iterate over quantities - over flux tube - over time readings
+    # write data into np.array[(#quantitites * #flux-tubes) x #readings] and to a dictionary{(quantities x flux-tubes):readings}
+    for i_q,(key,val) in enumerate(datadict.items()):
+
+        for n_ft in range(n_fts):
+
+            data_table_dict[dict_naming_dict[key]+str(n_ft+1)] = np.zeros(val.shape[0])
+
+            if 'transp' in key:
+                coord = n_ft
+            else:
+                coord = coord_num_fts[n_ft]
+
+            for it in range(val.shape[0]):
+
+                data_table[it, i_q*n_fts+n_ft] = val[it,coord]
+                data_table_dict[dict_naming_dict[key]+str(n_ft+1)][it] = val[it,coord]
+
+    # write CSV file with header of quantities x flux-tubes spearated by 2 spaces
+    with open(save_fold_name+'res_'+codename+'.csv', 'w') as csv_file:
+
+        writer = csv.writer(csv_file, delimiter=' ')
+        writer.writerow(data_table_dict.keys())
+
+        for r in data_table:
+
+            writer.writerow(r)
+    
+    return 0
+
 def read_profs(codename='gem_', dates=['20230823_151955'], prefix_name='workflow/run_fusion_', sufix_name='/instances/transport/workdir/', cpo_filebase='', cpo_names = ['coreprof', 'coretransp']):
 
     load_fold_names = [prefix_name + codename + \
@@ -136,6 +264,7 @@ def read_profs(codename='gem_', dates=['20230823_151955'], prefix_name='workflow
     n_rho_resol = 1
 
     bool_sur_involved = 'sur' in codename or 'multiimpl' in codename
+    bool_sur_involved = True
 
     # Load reference data for the surrogate training data set
 
@@ -165,7 +294,7 @@ def read_profs(codename='gem_', dates=['20230823_151955'], prefix_name='workflow
 
         if cpo_name == 'coreprof':
 
-            quantities = ['ti', 'te', 'ni', 'ne']
+            quantities = ['ti', 'te', 'ni', 'ne', ]
 
             attributes = ['ddrho', 'value', ]
 
@@ -182,9 +311,9 @@ def read_profs(codename='gem_', dates=['20230823_151955'], prefix_name='workflow
             j_a_s = [0, 1] # indiced of attributes to go through
 
         if cpo_name == 'coretransp':
-            quantities = ['ti_transp', 'te_transp']
+            quantities = ['ti_transp', 'te_transp', ]
 
-            attributes = ['flux', 'diff_eff', 'vconv_eff']
+            attributes = ['flux', 'diff_eff', 'vconv_eff', ]
 
             if codename == 'gem_surr':
                 #coord_num = [0]  # if n_fts==1
@@ -247,7 +376,9 @@ def read_profs(codename='gem_', dates=['20230823_151955'], prefix_name='workflow
             # Display the array of actual ETS time step lengths
             #print(f"> delta-t array for {quantities[i_q]+'_'+attributes[j_a]}: \n{times[1:]-times[:-1]}")
 
-            color_list = ['b', 'g', 'r', 'y' , 'm', 'c', 'k']
+            color_step = 0.05
+            #color_list = ['b', 'g', 'r', 'y' , 'm', 'c', 'k']
+            color_list = [(0.0, 0.0, color_step*i) for i in range(1,12)]
             line_list = ['-', '--', '-.', ':']
             marker_list = ['', '.', 'o', 'v', '^', '<', '>']
             style_lists = [marker_list, line_list, color_list,] 
@@ -314,63 +445,13 @@ def read_profs(codename='gem_', dates=['20230823_151955'], prefix_name='workflow
                         quantities[i_q]+'_'+attributes[j_a]+'_'+dates[0]+'_'+dates[-1]+'.pdf')
             plt.close()
 
-        ### Plotting all quantities against each other
-        # Iterate over all pairs of quantity names
-        for q_x,q_y in product(datadict.keys(), datadict.keys()):
+    ### Plotting all quantities against each other
 
-            #print(f"- datadict shapes: {datadict[q_x].shape} {datadict[q_y].shape}") ###DEBUG
+    plot_quantities(datadict=datadict, save_fold_name=save_fold_name, coord_num_fts=coord_num_fts, bool_sur_involved=bool_sur_involved, n_run_per_ft=n_run_per_ft, ref_data=ref_data)
 
-            #n_fts = min(datadict[q_x].shape[-1], datadict[q_y].shape[-1])
-            n_fts = 8 # extract that's it 8 flux tube from 100 rho coordinates :)
+    ### Write down a single csv for simulation results
 
-            # Iterate over all flux tubes
-            for n_ft in range(n_fts):
-            
-                fig, ax = plt.subplots()
-
-                t_min = min(len(datadict[q_x][:,n_ft]), len(datadict[q_y][:,n_ft]))
-
-                if 'transp' in q_x:
-                    coord_x = n_ft
-                else:
-                    coord_x = coord_num_fts[n_ft]
-                
-                if 'transp' in q_y:
-                    coord_y = n_ft
-                else:
-                    coord_y = coord_num_fts[n_ft]
-
-                ax.plot(datadict[q_x][:t_min,coord_x], 
-                        datadict[q_y][:t_min,coord_y],
-                        marker='.',
-                        )
-
-                # Add vertical and horisontal lines from a reference dataset
-                if bool_sur_involved:
-
-                    if q_y in ref_data.columns:
-                        y_min_val = ref_data[q_y].iloc[n_run_per_ft*(n_ft):n_run_per_ft*(n_ft+1)].min()
-                        y_max_val = ref_data[q_y].iloc[n_run_per_ft*(n_ft):n_run_per_ft*(n_ft+1)].max()
-                        ax.hlines(y=y_min_val, xmin=datadict[q_x][:,coord_x].min(), xmax=datadict[q_x][:,coord_x].max(), 
-                                color='r', linestyle='--', label='bounds of the training dataset')
-                        ax.hlines(y=y_max_val, xmin=datadict[q_x][:,coord_x].min(), xmax=datadict[q_x][:,coord_x].max(),
-                                color='r', linestyle='--')
-                    
-                    if q_x in ref_data.columns:
-                        x_min_val = ref_data[q_x].iloc[n_run_per_ft*(n_ft):n_run_per_ft*(n_ft+1)].min()
-                        x_max_val = ref_data[q_x].iloc[n_run_per_ft*(n_ft):n_run_per_ft*(n_ft+1)].max()
-                    
-                        ax.vlines(x=x_min_val, ymin=datadict[q_y][:,coord_y].min(), ymax=datadict[q_y][:,coord_y].max(), 
-                                color='r', linestyle='--',)
-                        ax.vlines(x=x_max_val, ymin=datadict[q_y][:,coord_y].min(), ymax=datadict[q_y][:,coord_y].max(),
-                                color='r', linestyle='--')
-
-                ax.set_xlabel(q_x)
-                ax.set_ylabel(q_y)
-                fig.savefig(save_fold_name+'quant_'+codename+'_' +
-                            q_x+'_'+q_y+'_'+'ft'+str(n_ft)+'_'+dates[0]+'_'+dates[-1]+'.pdf')
-                plt.close()
-
+    write_table_csv(datadict=datadict, save_fold_name=save_fold_name)
 
     return dates
 
@@ -389,7 +470,7 @@ if __name__ == '__main__':
     
     dates = read_profs(codename=codename, dates=dates) # to read results of M3-WF run
     
-    #dates = read_profs(codename='', dates=['20230928', '20230929', '20231004', '20231006'], prefix_name='../standalone/bin/gem_develop_turbulence/', sufix_name='/', cpo_filebase='gem_', cpo_names=['coretransp']) # to read from stanalone runs
+    #dates = read_profs(codename='', dates=['20230928', '20230929', '20231004', '20231006', '20231009'], prefix_name='../standalone/bin/gem_develop_turbulence/', sufix_name='/', cpo_filebase='gem_', cpo_names=['coretransp']) # to read from stanalone runs
 
 
     #dates = ['20230818_135913', '20230821_161005', '20230822_150943', '20230823_151955', '20230824', '20230825', '20230828', '20230829', '20230830', '20230831', '20230901']
