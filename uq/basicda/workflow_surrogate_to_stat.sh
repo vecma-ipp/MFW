a6bff83618f82da6eb2755c76fd113dbf43ea721
Yehor Yudin <yehor.yudin@ipp.mpg.de>
2024-02-06 19:44:01 +0100

    Surrogate retraining loop shell script

--- uq/basicda/workflow_surrogate_to_stat.sh
+++ uq/basicda/workflow_surrogate_to_stat.sh
@@ -1,49 +1,86 @@
 #!/bin/sh
 
+#Define fixed folder and file names
+muscledir=~/code/MFW/muscle3
+muscleworkdir=workflow
+runprefix=run_fusion_gem0_surr_
+runsufix=/instances/transport/workdir
+
+easysurrogatedir=~/code/EasySurrogate/tests/gem_gp
+
+lastcoreprofcpo=ets_coreprof_out.cpo
+lastequilibriumcpo=equilupdate_equilibrium_out.cpo
+
+#Define script file names of each operation
+prepare_op=prepare_gem0_sample.py
+surrogate_op=process_gpr.sh
+simulation_op=gem_surr_workflow.sh
+
+# Define the starting point of the loop - and prepare the first iteration
+#runid_prev=20240202_2
+sourcedir=${muscledir}/${muscleworkdir}/gem0_surr_resume_data/
+cp ${sourcedir}/ets_coreprof_in.cpo ${sourcedir}/${lastcoreprofcpo}
+cp ${sourcedir}/ets_equilibrium_in.cpo ${sourcedir}/${lastequilibriumcpo}
+
 datenow=$(date +"%Y%m%d")
 
+#TODO make a folder for every iteration
 origdir=$(pwd)
 
-curr_id=datenow
+curr_id=${datenow}
 
-### Prepare data for an iteration: CPOs 
-itnum=0
-itdir=prep_folder_${datenow}_${itnum}
-mkdir ${itdir}
+itnum_min=0
+itnum_max=2
 
-muscledir=~/code/MFW/muscle3/workflow/
-runprefix=run_fusion_gem0_surr_
-runsufix=/instances/transport/workdir/
+for((itnum=${itnum_min};itnum<${itnum_max};itnum++)); do
+  
+  ### Prepare data for an iteration: CPOs 
+  
+  itdir=folder_${datenow}_${itnum}
+  #ATTENTION: override for testing
+  itdir=${PWD}
+  #mkdir ${itdir}
 
-runid=20240202_2
-sourcedir=${muscledir}${runprefix}${runid}${runsufix}
+  cp ${sourcedir}/${lastequilibriumcpo} ${itdir}/
+  cp ${sourcedir}/${lastcoreprofcpo} ${itdir}/
 
-#ATTENTION: override for testing
-itdir=${PWD}
+  ### Prepare run folder:
+  # 	 final coreprof CPO -> final plasma state -> grid around final state -> pyGEM0 dataset around final state
+  
+  python ${prepare_op} ${itdir} ${curr_id} ${itnum}
 
-#TODO iterate over CPO definitions
-cp ${sourcedir}/equilupdate_equilibrium_out.cpo ${itdir}/
-cp ${sourcedir}/ets_coreprof_out.cpo    ${itdir}/
+  ### Prepare new surrogate with new data
 
-# Prepare run folder -> final coreprof CPO -> final plasma state -> grid around final state -> pyGEM0 dataset around final state
-python prepare_gem0_sample.py ${itdir} ${curr_id} ${itnum}
+  cd ${easysurrogatedir}
 
-### Prepare new surrogate with new data
+   #TODO fix the GP analysis error due to vector output  
+  ./${surrogate_op} ${datenow} ${itnum} 
 
-cd ~/code/EasySurrogate/tests/gem_gp/
-#TODO modify the shell file for GPR
-process_gpr.sh ${datenow} ${itnum} 
+  ### Run M3-WF with the new surrogate
+  cd ${muscledir}/${muscleworkdir}/
+ 
+  # DEBUG: delete old M3-WF directory
+  rm -r ${runprefix}${datenow}_${itnum} 
 
-### Run M3-WF with the new surrogate
-cd ~/code/MFW/muscle3/workflow/
-#TODO modify the shell script for M3-WF
-./gem_surr_workflow.sh ${datenow} ${itnum}
+  # Prepare the necessary files
+  cp ${orig_dir}/gem0py_new_${curr_id}_${itnum}.csv ${muscledir}/ref_train_data.csv
+  # Original state for the new M3-WF run - two options:
+  # 	1) Same inial state (~ from AUG shot)
+  # 	2) Final state of the WF from the previous iteration
+  # Choice - do not change the intial state
 
-### Collect the data from the M3-WF run and compare resulting profiles with the last iteration
-cp run_fusion_gem0_surr_${datenow}_${itnum}/instances/transport/workdir/etc_coreprof_out.cpo ${origdir}/
-cd ${origdir}
+  ./${simulation_op} ${datenow} ${itnum}
 
-#conv=$(python compare_coreprof.py ${itnum})
+  ### Collect the data from the M3-WF run and compare resulting profiles with the last iteration
+  cp ${runprefix}${datenow}_${itnum}${runsufix}/${lastcoreprofcpo} ${origdir}/
+  cd ${origdir}
 
+  #TODO analyse the 'final state' of the iteration
+  #conv=$(python compare_coreprof.py ${itnum})
+  
+  ### Prepare for the next iteration
+  runid_prev=${datenow}_${itnum}
+  sourcedir=${muscledir}/${muscleworkdir}/${runprefix}${runid_prev}${runsufix}/
 
+done
 
