243327d8420bf953bf148e92de3af24566a5641e
Yehor Yudin <yehor.yudin@ipp.mpg.de>
2024-02-16 20:15:33 +0100

    Changes in oldf profile plotting script

--- plots/plot-profiles.py
+++ plots/plot-profiles.py
@@ -1,45 +1,113 @@
 import pylab
 import ascii_cpo
 
-corep = ascii_cpo.read("ets_coreprof_in.cpo","coreprof")
+#corep = ascii_cpo.read("ets_coreprof_in.cpo","coreprof")
+#corep = ascii_cpo.read("gem0_coreprof_in_2.cpo","coreprof")
+#corep = ascii_cpo.read("gem_resume_coreprof_in.cpo","coreprof")
+#corep = ascii_cpo.read("ets_coreprof_in_gem0_20231023.cpo", "coreprof")
+#corep = ascii_cpo.read("gem_coreprof_aug28906_6rest_in.cpo", "coreprof")
 
+label_list = ['surrogate', 'GEM0']
+cpo_filename_list = ['ets_coreprof_out_surr_wf_20240216.cpo', 'ets_coreprof_out_gem0_wf_20240216.cpo']
+corep_list = [ascii_cpo.read(cpo_filename, "coreprof") for cpo_filename in cpo_filename_list]
 
-pylab.figure(1)
-pylab.suptitle("Initial profiles for JET#92436/270")
+pylab.figure(figsize=(7,7))
+
+#pylab.suptitle("Initial profiles for JET#92436/270")
+#pylab.suptitle("Initial profiles for AUG#28906/6")
+#pylab.suptitle("Initial profiles for AUG#28906/6/restart(8ft)")
+
+pylab.suptitle("Final profiles for MFW simulation of AUG#28906/6/")
+
+# pylab.subplot(321)
+# for corep,label in zip(corep_list,label_list):
+#     pylab.plot(corep.rho_tor_norm,corep.profiles1d.q.value, label=label, alpha=0.5)
+# pylab.yscale('symlog')
+# #pylab.xlabel("rho_tor_norm")
+# pylab.xlabel(f"$\\rho_{{tor}}^{{norm}}$")
+# pylab.ylabel(f"$q$")
+# pylab.legend(loc='best')
+
+# pylab.subplot(322)
+# for corep,label in zip(corep_list,label_list):
+#     pylab.plot(corep.rho_tor_norm,corep.profiles1d.pr_th.value, label=label, alpha=0.5)
+# #pylab.xlabel("rho_tor_norm")
+# pylab.xlabel(f"$\\rho_{{tor}}^{{norm}}$")
+# pylab.ylabel(f"$pr_{{th}}$")
+# pylab.legend(loc='best')
+
+# pylab.subplot(322)
+# for corep,label in zip(corep_list,label_list):
+#     pylab.plot(corep.rho_tor_norm,corep.profiles1d.gm3.value, label=label, alpha=0.5)
+# pylab.yscale('symlog')
+# pylab.xlabel(f"$\\rho_{{tor}}^{{norm}}$")
+# pylab.ylabel(f"$pr_{{th}}$")
+# pylab.legend(loc='best')
 
 pylab.subplot(321)
-pylab.plot(corep.rho_tor_norm,corep.profiles1d.q.value)
+for corep,label in zip(corep_list,label_list):
+    pylab.plot(corep.rho_tor_norm,corep.te.value, label=label, alpha=0.5)
+pylab.yscale('symlog')
+pylab.grid()
 #pylab.xlabel("rho_tor_norm")
-pylab.ylabel("q")
+pylab.xlabel(f"$\\rho_{{tor}}^{{norm}}$")
+pylab.ylabel(f"$T_{{e}}, eV$")
+pylab.legend(loc='best')
 
 pylab.subplot(322)
-pylab.plot(corep.rho_tor_norm,corep.profiles1d.pr_th.value)
+for corep,label in zip(corep_list,label_list):
+    pylab.plot(corep.rho_tor_norm,corep.ti.value, label=label, alpha=0.5)
+pylab.yscale('symlog')
+pylab.grid()
 #pylab.xlabel("rho_tor_norm")
-pylab.ylabel("pr_th")
+pylab.xlabel(f"$\\rho_{{tor}}^{{norm}}$")
+pylab.ylabel(f"$T_{{i}}, eV$")
+pylab.legend(loc='best')
 
 pylab.subplot(323)
-pylab.plot(corep.rho_tor_norm,corep.te.value)
+for corep,label in zip(corep_list,label_list):
+    pylab.plot(corep.rho_tor_norm,corep.te.ddrho, label=label, alpha=0.5)
+pylab.yscale('symlog')
+pylab.grid()
 #pylab.xlabel("rho_tor_norm")
-pylab.ylabel("te")
+pylab.xlabel(f"$\\rho_{{tor}}^{{norm}}$")
+pylab.ylabel(f"$\\nabla T_{{e}}, eV/m$")
+pylab.legend(loc='best')
 
 pylab.subplot(324)
-pylab.plot(corep.rho_tor_norm,corep.ti.value)
+for corep,label in zip(corep_list,label_list):
+    pylab.plot(corep.rho_tor_norm,corep.ti.ddrho, label=label, alpha=0.5)
+pylab.yscale('symlog')
+pylab.grid()
 #pylab.xlabel("rho_tor_norm")
-pylab.ylabel("ti")
+pylab.xlabel(f"$\\rho_{{tor}}^{{norm}}$")
+pylab.ylabel(f"$\\nabla T_{{i}}$, eV/m")
+pylab.legend(loc='best')
 
 pylab.subplot(325)
-pylab.plot(corep.rho_tor_norm,corep.ne.value)
-pylab.xlabel("rho_tor_norm")
-pylab.ylabel("ne")
+for corep,label in zip(corep_list,label_list):
+    pylab.plot(corep.rho_tor_norm,corep.ne.value, label=label, alpha=0.5)
+pylab.yscale('symlog')
+pylab.grid()
+#pylab.xlabel("rho_tor_norm")
+pylab.xlabel(f"$\\rho_{{tor}}^{{norm}}$")
+pylab.ylabel(f"$n_{{e}}$")
+pylab.legend(loc='best')
 
 pylab.subplot(326)
-pylab.plot(corep.rho_tor_norm,corep.ni.value)
-pylab.xlabel("rho_tor_norm")
-pylab.ylabel("ni")
+for corep,label in zip(corep_list,label_list):
+    pylab.plot(corep.rho_tor_norm,corep.ni.value, label=label, alpha=0.5)
+pylab.yscale('symlog')
+pylab.grid()
+pylab.tick_params(axis='y')
+#pylab.xlabel("rho_tor_norm")
+pylab.xlabel(f"$\\rho_{{tor}}^{{norm}}$")
+pylab.ylabel(f"$n_{{i}}$")
+pylab.legend(loc='best')
 
 pylab.tight_layout()
+#pylab.show(block=True)
 
-pylab.show(block=True)
-
+pylab.savefig('prof_gem0_20240216.pdf')
 
 
